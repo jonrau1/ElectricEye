0d3c8a1dd514abca500faa1527a23d4145b38ea0
jonrau1 <46727149+jonrau1@users.noreply.github.com>
2020-03-04 09:06:31 -0500

    Add 4 new Playbooks. Update CFN permissions. Update readmes. Change names on some playbook files

---
+++ add-ons/electriceye-response/raw-source/DocDB_Privatize_Snapshot_Playbook.py
@@ -0,0 +1,82 @@
+# This file is part of ElectricEye.
+
+# ElectricEye is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# ElectricEye is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with ElectricEye.  
+# If not, see https://github.com/jonrau1/ElectricEye/blob/master/LICENSE.
+import boto3
+import os
+
+def lambda_handler(event, context):
+    # boto3 clients
+    sts = boto3.client('sts')
+    securityhub = boto3.client('securityhub')
+    # create env vars
+    awsRegion = os.environ['AWS_REGION']
+    lambdaFunctionName = os.environ['AWS_LAMBDA_FUNCTION_NAME']
+    masterAccountId = sts.get_caller_identity()['Account']
+    # parse ASFF
+    securityHubEvent = (event['detail']['findings'])
+    for findings in securityHubEvent:
+        # parse finding ID
+        findingId =str(findings['Id'])
+        # parse Account from SecHub Finding
+        findingOwner = str(findings['AwsAccountId'])
+        for resources in findings['Resources']:
+            docDbSnapshotId = str(resources['Details']['Other']['SnapshotId'])
+            if findingOwner != masterAccountId:
+                memberAcct = sts.assume_role(RoleArn='arn:aws:iam::' + findingOwner + ':role/XA-ElectricEye-Response',RoleSessionName='x_acct_sechub')
+                # retrieve creds from member account
+                xAcctAccessKey = memberAcct['Credentials']['AccessKeyId']
+                xAcctSecretKey = memberAcct['Credentials']['SecretAccessKey']
+                xAcctSeshToken = memberAcct['Credentials']['SessionToken']
+                # create service client using the assumed role credentials
+                documentdb = boto3.client('docdb',aws_access_key_id=xAcctAccessKey,aws_secret_access_key=xAcctSecretKey,aws_session_token=xAcctSeshToken)
+                try:
+                    # remove public sharing from DocDB cluster snapshot
+                    response = documentdb.modify_db_cluster_snapshot_attribute(
+                        DBClusterSnapshotIdentifier=docDbSnapshotId,
+                        AttributeName='restore',
+                        ValuesToRemove=['all']
+                    )
+                    print(response)
+                    try:
+                        response = securityhub.update_findings(
+                            Filters={'Id': [{'Value': findingId,'Comparison': 'EQUALS'}]},
+                            Note={'Text': 'DocumentDB Cluster has had deletion protection enabled and the finding was archived. Review the cluster to identify any other misconfigurations','UpdatedBy': lambdaFunctionName},
+                            RecordState='ARCHIVED'
+                        )
+                        print(response)
+                    except Exception as e:
+                        print(e)
+                except Exception as e:
+                    print(e)
+            else:
+                try:
+                    documentdb = boto3.client('docdb')
+                    # remove public sharing from DocDB cluster snapshot
+                    response = documentdb.modify_db_cluster_snapshot_attribute(
+                        DBClusterSnapshotIdentifier=docDbSnapshotId,
+                        AttributeName='restore',
+                        ValuesToRemove=['all']
+                    )
+                    print(response)
+                    try:
+                        response = securityhub.update_findings(
+                            Filters={'Id': [{'Value': findingId,'Comparison': 'EQUALS'}]},
+                            Note={'Text': 'DocumentDB Cluster has had deletion protection enabled and the finding was archived. Review the cluster to identify any other misconfigurations','UpdatedBy': lambdaFunctionName},
+                            RecordState='ARCHIVED'
+                        )
+                        print(response)
+                    except Exception as e:
+                        print(e)
+                except Exception as e:
+                    print(e)
---
+++ add-ons/electriceye-response/raw-source/Remove_Open_DocDB_Playbook.py
@@ -0,0 +1,76 @@
+# This file is part of ElectricEye.
+
+# ElectricEye is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# ElectricEye is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with ElectricEye.  
+# If not, see https://github.com/jonrau1/ElectricEye/blob/master/LICENSE.
+import boto3
+import os
+
+def lambda_handler(event, context):
+    # boto3 clients
+    sts = boto3.client('sts')
+    securityhub = boto3.client('securityhub')
+    # create env vars
+    awsRegion = os.environ['AWS_REGION']
+    lambdaFunctionName = os.environ['AWS_LAMBDA_FUNCTION_NAME']
+    masterAccountId = sts.get_caller_identity()['Account']
+    # parse Security Hub CWE
+    securityHubEvent = (event['detail']['findings'])
+    for findings in securityHubEvent:
+        # parse finding ID
+        findingId =str(findings['Id'])
+        # parse Account from SecHub Finding
+        findingOwner = str(findings['AwsAccountId'])
+        for resources in findings['Resources']:
+            resourceId = str(resources['Id'])
+            # create resource ID
+            securityGroupId = resourceId.replace('arn:aws:ec2:' + awsRegion + ':' + findingOwner + ':security-group/', '')
+            if findingOwner != masterAccountId:
+                memberAcct = sts.assume_role(RoleArn='arn:aws:iam::' + findingOwner + ':role/XA-ElectricEye-Response',RoleSessionName='x_acct_sechub')
+                # retrieve creds from member account
+                xAcctAccessKey = memberAcct['Credentials']['AccessKeyId']
+                xAcctSecretKey = memberAcct['Credentials']['SecretAccessKey']
+                xAcctSeshToken = memberAcct['Credentials']['SessionToken']
+                # create service client using the assumed role credentials
+                ec2 = boto3.resource('ec2',aws_access_key_id=xAcctAccessKey,aws_secret_access_key=xAcctSecretKey,aws_session_token=xAcctSeshToken)
+                security_group = ec2.SecurityGroup(securityGroupId)
+                try:
+                    # remove 0.0.0.0 access to 27017
+                    response = security_group.revoke_ingress(IpPermissions=[{'IpProtocol': 'tcp','FromPort': 27017,'ToPort': 27017,'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}],DryRun=False)
+                    try:
+                        response = securityhub.update_findings(
+                            Filters={'Id': [{'Value': findingId,'Comparison': 'EQUALS'}]},
+                            Note={'Text': 'Removed open access to DocumentDB (TCP 27017) from Security Group and archived the finding. Review the Security Group for any other unauthorized port changes.','UpdatedBy': lambdaFunctionName},
+                            RecordState='ARCHIVED'
+                        )
+                        print(response)
+                    except Exception as e:
+                        print(e)
+                except Exception as e:
+                    print(e)
+            else:
+                try:
+                    ec2 = boto3.resource('ec2')
+                    security_group = ec2.SecurityGroup(securityGroupId)
+                    # remove 0.0.0.0 access to 27017
+                    response = security_group.revoke_ingress(IpPermissions=[{'IpProtocol': 'tcp','FromPort': 27017,'ToPort': 27017,'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}],DryRun=False)
+                    try:
+                        response = securityhub.update_findings(
+                            Filters={'Id': [{'Value': findingId,'Comparison': 'EQUALS'}]},
+                            Note={'Text': 'Removed open access to DocumentDB (TCP 27017) from Security Group and archived the finding. Review the Security Group for any other unauthorized port changes.','UpdatedBy': lambdaFunctionName},
+                            RecordState='ARCHIVED'
+                        )
+                        print(response)
+                    except Exception as e:
+                        print(e)
+                except Exception as e:
+                    print(e)
