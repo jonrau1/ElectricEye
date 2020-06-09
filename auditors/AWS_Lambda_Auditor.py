b9f87554f98635ed1f458007a7773399cc1ae8f8
Jody Brazil <jody.brazil@gmail.com>
2020-06-09 09:35:07 -0500

    try block not necessary - not calling sechub

--- auditors/AWS_Lambda_Auditor.py
+++ auditors/AWS_Lambda_Auditor.py
@@ -58,122 +58,112 @@ class FunctionUnusedCheck(Auditor):
                 date_delta = datetime.datetime.now(datetime.timezone.utc) - modify_date
                 iso8601Time = datetime.datetime.now(datetime.timezone.utc).isoformat()
                 if len(metric["Values"]) > 0 or date_delta.days < 30:
-                    try:
-                        finding = {
-                            "SchemaVersion": "2018-10-08",
-                            "Id": lambdaArn + "/lambda-function-unused-check",
-                            "ProductArn": "arn:aws:securityhub:"
-                            + awsRegion
-                            + ":"
-                            + awsAccountId
-                            + ":product/"
-                            + awsAccountId
-                            + "/default",
-                            "GeneratorId": lambdaArn,
-                            "AwsAccountId": awsAccountId,
-                            "Types": [
-                                "Software and Configuration Checks/AWS Security Best Practices"
+                    finding = {
+                        "SchemaVersion": "2018-10-08",
+                        "Id": lambdaArn + "/lambda-function-unused-check",
+                        "ProductArn": "arn:aws:securityhub:"
+                        + awsRegion
+                        + ":"
+                        + awsAccountId
+                        + ":product/"
+                        + awsAccountId
+                        + "/default",
+                        "GeneratorId": lambdaArn,
+                        "AwsAccountId": awsAccountId,
+                        "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
+                        "FirstObservedAt": iso8601Time,
+                        "CreatedAt": iso8601Time,
+                        "UpdatedAt": iso8601Time,
+                        "Severity": {"Label": "INFORMATIONAL"},
+                        "Confidence": 99,
+                        "Title": "[Lambda.1] Lambda functions should be deleted after 30 days of no use",
+                        "Description": "Lambda function "
+                        + functionName
+                        + " has been used or updated in the last 30 days.",
+                        "Remediation": {
+                            "Recommendation": {
+                                "Text": "For more information on best practices for lambda functions refer to the Best Practices for Working with AWS Lambda Functions section of the Amazon Lambda Developer Guide",
+                                "Url": "https://docs.aws.amazon.com/lambda/latest/dg/best-practices.html#function-configuration",
+                            }
+                        },
+                        "ProductFields": {"Product Name": "ElectricEye"},
+                        "Resources": [
+                            {
+                                "Type": "AwsLambda",
+                                "Id": lambdaArn,
+                                "Partition": "aws",
+                                "Region": awsRegion,
+                            }
+                        ],
+                        "Compliance": {
+                            "Status": "PASSED",
+                            "RelatedRequirements": [
+                                "NIST CSF ID.AM-2",
+                                "NIST SP 800-53 CM-8",
+                                "NIST SP 800-53 PM-5",
+                                "AICPA TSC CC3.2",
+                                "AICPA TSC CC6.1",
+                                "ISO 27001:2013 A.8.1.1",
+                                "ISO 27001:2013 A.8.1.2",
+                                "ISO 27001:2013 A.12.5.1",
                             ],
-                            "FirstObservedAt": iso8601Time,
-                            "CreatedAt": iso8601Time,
-                            "UpdatedAt": iso8601Time,
-                            "Severity": {"Label": "INFORMATIONAL"},
-                            "Confidence": 99,
-                            "Title": "[Lambda.1] Lambda functions should be deleted after 30 days of no use",
-                            "Description": "Lambda function "
-                            + functionName
-                            + " has been used or updated in the last 30 days.",
-                            "Remediation": {
-                                "Recommendation": {
-                                    "Text": "For more information on best practices for lambda functions refer to the Best Practices for Working with AWS Lambda Functions section of the Amazon Lambda Developer Guide",
-                                    "Url": "https://docs.aws.amazon.com/lambda/latest/dg/best-practices.html#function-configuration",
-                                }
-                            },
-                            "ProductFields": {"Product Name": "ElectricEye"},
-                            "Resources": [
-                                {
-                                    "Type": "AwsLambda",
-                                    "Id": lambdaArn,
-                                    "Partition": "aws",
-                                    "Region": awsRegion,
-                                }
-                            ],
-                            "Compliance": {
-                                "Status": "PASSED",
-                                "RelatedRequirements": [
-                                    "NIST CSF ID.AM-2",
-                                    "NIST SP 800-53 CM-8",
-                                    "NIST SP 800-53 PM-5",
-                                    "AICPA TSC CC3.2",
-                                    "AICPA TSC CC6.1",
-                                    "ISO 27001:2013 A.8.1.1",
-                                    "ISO 27001:2013 A.8.1.2",
-                                    "ISO 27001:2013 A.12.5.1",
-                                ],
-                            },
-                            "Workflow": {"Status": "RESOLVED"},
-                            "RecordState": "ARCHIVED",
-                        }
-                        yield finding
-                    except Exception as e:
-                        print(e)
+                        },
+                        "Workflow": {"Status": "RESOLVED"},
+                        "RecordState": "ARCHIVED",
+                    }
+                    yield finding
                 else:
-                    try:
-                        finding = {
-                            "SchemaVersion": "2018-10-08",
-                            "Id": lambdaArn + "/lambda-function-unused-check",
-                            "ProductArn": "arn:aws:securityhub:"
-                            + awsRegion
-                            + ":"
-                            + awsAccountId
-                            + ":product/"
-                            + awsAccountId
-                            + "/default",
-                            "GeneratorId": lambdaArn,
-                            "AwsAccountId": awsAccountId,
-                            "Types": [
-                                "Software and Configuration Checks/AWS Security Best Practices"
+                    finding = {
+                        "SchemaVersion": "2018-10-08",
+                        "Id": lambdaArn + "/lambda-function-unused-check",
+                        "ProductArn": "arn:aws:securityhub:"
+                        + awsRegion
+                        + ":"
+                        + awsAccountId
+                        + ":product/"
+                        + awsAccountId
+                        + "/default",
+                        "GeneratorId": lambdaArn,
+                        "AwsAccountId": awsAccountId,
+                        "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
+                        "FirstObservedAt": iso8601Time,
+                        "CreatedAt": iso8601Time,
+                        "UpdatedAt": iso8601Time,
+                        "Severity": {"Label": "LOW"},
+                        "Confidence": 99,
+                        "Title": "[Lambda.1] Lambda functions should be deleted after 30 days of no use",
+                        "Description": "Lambda function "
+                        + functionName
+                        + " has not been used or updated in the last 30 days.",
+                        "Remediation": {
+                            "Recommendation": {
+                                "Text": "For more information on best practices for lambda functions refer to the Best Practices for Working with AWS Lambda Functions section of the Amazon Lambda Developer Guide",
+                                "Url": "https://docs.aws.amazon.com/lambda/latest/dg/best-practices.html#function-configuration",
+                            }
+                        },
+                        "ProductFields": {"Product Name": "ElectricEye"},
+                        "Resources": [
+                            {
+                                "Type": "AwsLambda",
+                                "Id": lambdaArn,
+                                "Partition": "aws",
+                                "Region": awsRegion,
+                            }
+                        ],
+                        "Compliance": {
+                            "Status": "FAILED",
+                            "RelatedRequirements": [
+                                "NIST CSF ID.AM-2",
+                                "NIST SP 800-53 CM-8",
+                                "NIST SP 800-53 PM-5",
+                                "AICPA TSC CC3.2",
+                                "AICPA TSC CC6.1",
+                                "ISO 27001:2013 A.8.1.1",
+                                "ISO 27001:2013 A.8.1.2",
+                                "ISO 27001:2013 A.12.5.1",
                             ],
-                            "FirstObservedAt": iso8601Time,
-                            "CreatedAt": iso8601Time,
-                            "UpdatedAt": iso8601Time,
-                            "Severity": {"Label": "LOW"},
-                            "Confidence": 99,
-                            "Title": "[Lambda.1] Lambda functions should be deleted after 30 days of no use",
-                            "Description": "Lambda function "
-                            + functionName
-                            + " has not been used or updated in the last 30 days.",
-                            "Remediation": {
-                                "Recommendation": {
-                                    "Text": "For more information on best practices for lambda functions refer to the Best Practices for Working with AWS Lambda Functions section of the Amazon Lambda Developer Guide",
-                                    "Url": "https://docs.aws.amazon.com/lambda/latest/dg/best-practices.html#function-configuration",
-                                }
-                            },
-                            "ProductFields": {"Product Name": "ElectricEye"},
-                            "Resources": [
-                                {
-                                    "Type": "AwsLambda",
-                                    "Id": lambdaArn,
-                                    "Partition": "aws",
-                                    "Region": awsRegion,
-                                }
-                            ],
-                            "Compliance": {
-                                "Status": "FAILED",
-                                "RelatedRequirements": [
-                                    "NIST CSF ID.AM-2",
-                                    "NIST SP 800-53 CM-8",
-                                    "NIST SP 800-53 PM-5",
-                                    "AICPA TSC CC3.2",
-                                    "AICPA TSC CC6.1",
-                                    "ISO 27001:2013 A.8.1.1",
-                                    "ISO 27001:2013 A.8.1.2",
-                                    "ISO 27001:2013 A.12.5.1",
-                                ],
-                            },
-                            "Workflow": {"Status": "NEW"},
-                            "RecordState": "ACTIVE",
-                        }
-                        yield finding
-                    except Exception as e:
-                        print(e)
+                        },
+                        "Workflow": {"Status": "NEW"},
+                        "RecordState": "ACTIVE",
+                    }
+                    yield finding
