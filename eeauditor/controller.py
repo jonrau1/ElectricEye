33dbdef4350876204e1173af3cf2b9f465585fd7
Jody Brazil <jody.brazil@gmail.com>
2020-06-15 21:41:04 -0500

    separate controller and auditor

--- eeauditor/controller.py
+++ eeauditor/controller.py
@@ -13,84 +13,16 @@
 # You should have received a copy of the GNU General Public License along with ElectricEye.
 # If not, see https://github.com/jonrau1/ElectricEye/blob/master/LICENSE.
 
-from functools import partial
 import getopt
-import json
 import os
-import report
 import sys
+
 import boto3
-from pluginbase import PluginBase
-from check_register import CheckRegister
 
-
-sts = boto3.client("sts")
-
-here = os.path.abspath(os.path.dirname(__file__))
-get_path = partial(os.path.join, here)
-
-
-class EEAuditor(object):
-    """ElectricEye controller
-    
-        Load and execute all auditor plugins.
-    """
-
-    def __init__(self, name, search_path=None):
-        if not search_path:
-            search_path = "./auditors/aws"
-        self.name = name
-        self.plugin_base = PluginBase(package="electriceye")
-        # each check must be decorated with the @registry.register_check("cache_name")
-        # to be discovered during plugin loading.
-        self.registry = CheckRegister()
-        # vendor specific credentials dictionary
-        self.awsAccountId = sts.get_caller_identity()["Account"]
-        self.awsRegion = os.environ["AWS_REGION"]
-        # If there is a desire to add support for multiple clouds, this would be
-        # a great place to implement it.
-        self.source = self.plugin_base.make_plugin_source(
-            searchpath=[get_path(search_path)], identifier=self.name
-        )
-
-    def load_plugins(self, plugin_name):
-        if plugin_name:
-            try:
-                plugin = self.source.load_plugin(plugin_name)
-            except Exception as e:
-                print(f"Failed to load plugin {plugin_name} with exception {e}")
-        else:
-            for plugin_name in self.source.list_plugins():
-                try:
-                    plugin = self.source.load_plugin(plugin_name)
-                except Exception as e:
-                    print(f"Failed to load plugin {plugin_name} with exception {e}")
-
-    def run_checks(self, requested_check_name):
-        for cache_name, cache in self.registry.checks.items():
-            # a dictionary to be used by checks that share a common cache
-            auditor_cache = {}
-            for check_name, check in cache.items():
-                # if a specific check is requested, only run that one check
-                if (
-                    not requested_check_name
-                    or requested_check_name
-                    and requested_check_name == check_name
-                ):
-                    try:
-                        print(f"Executing check {self.name}.{check_name}")
-                        for finding in check(
-                            cache=auditor_cache,
-                            awsAccountId=self.awsAccountId,
-                            awsRegion=self.awsRegion,
-                        ):
-                            yield finding
-                    except Exception as e:
-                        print(f"Failed to execute check {check_name} with exception {e}")
+from eeauditor import EEAuditor
 
 
 def main(argv):
-    findings_list = []  # used if --output is specified
     profile_name = ""
     auditor_name = ""
     check_name = ""
@@ -127,37 +59,8 @@ def main(argv):
 
     app = EEAuditor(name="AWS Auditor")
     app.load_plugins(plugin_name=auditor_name)
-
-    # TODO: currently streaming all findings to a statically defined file on the file
-    # system.  Should support a custom file name.
-    # TODO: Consider removing up this file after execution if the user doesn't ask to
-    # persist the output as a json file.
-    first = True
-    json_out_location = ""
-    with open("findings.json", "w") as json_out:
-        print('{"Findings":[', file=json_out)
-        json_out_location = os.path.abspath(json_out.name)
-        for result in app.run_checks(requested_check_name=check_name):
-            # print a comma separation between findings except before first finding
-            if first:
-                first = False
-            else:
-                print(",", file=json_out)
-            json.dump(result, json_out, indent=2)
-        print("]}", file=json_out)
-    json_out.close()
-    if sechub:
-        print("Writing results to SecurityHub")
-        securityhub = boto3.client("securityhub")
-        with open(json_out_location) as read_json_findings:
-            findings = json.load(read_json_findings)
-            securityhub.batch_import_findings(Findings=findings["Findings"])
-        read_json_findings.close()
-    else:
-        print("Not writing results to SecurityHub")
-    if output:
-        report.csv_output(input_file=json_out_location, output_file=output_file)
-    print("Done")
+    json_out = app.run(sechub=sechub, output=output, check_name=check_name)
+    print(f"Done.  Raw results {json_out}")
 
 
 if __name__ == "__main__":
