c394f81daa35f0ee363575f94da78a3bb957d511
Jody Brazil <jody.brazil@gmail.com>
2020-06-19 22:06:50 -0500

    remove test that is no longer valid

--- eeauditor/tests/test_eeauditor.py
+++ eeauditor/tests/test_eeauditor.py
@@ -36,15 +36,3 @@ def test_eeauditor_plugin_run_one_check():
     app.load_plugins(plugin_name="plugin1")
     for result in app.run_checks(requested_check_name="plugin_func_1"):
         assert result == {"SchemaVersion": "2018-10-08", "Id": "test-finding"}
-
-
-def test_eeauditor_run():
-    app = EEAuditor(name="test controller", search_path="./tests/test_modules")
-    app.load_plugins(plugin_name="plugin1")
-    output_file = app.run(sechub=False, output=False, check_name="plugin_func_1")
-    with open(output_file) as f:
-        findings_file = json.load(f)
-        findings = findings_file["Findings"]
-        print(findings)
-        assert findings == [{"SchemaVersion": "2018-10-08", "Id": "test-finding"}]
-    f.close()
