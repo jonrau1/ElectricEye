e38718808367f70437dc455163be5f9431c8b86f
Jody Brazil <jody.brazil@gmail.com>
2020-06-22 07:27:23 -0600

    move environ var colleciton out of init

--- eeauditor/processor/outputs/dops.py
+++ eeauditor/processor/outputs/dops.py
@@ -3,6 +3,9 @@ import os
 import requests
 from processor.outputs.output_base import ElectricEyeOutput
 
+client_id = os.environ.get("DOPS_CLIENT_ID", None)
+api_key = os.environ.get("DOPS_API_KEY", None)
+
 
 @ElectricEyeOutput
 class DopsProvider(object):
@@ -10,14 +13,12 @@ class DopsProvider(object):
 
     def __init__(self):
         self.url = "https://collector.dev2.disruptops.com/event"
-        self.client_id = os.environ.get("DOPS_CLIENT_ID")
-        self.api_key = os.environ.get("DOPS_API_KEY")
 
     def write_findings(self, findings: list, **kwargs):
-        if self.client_id and self.api_key:
+        if client_id and api_key:
             for finding in findings:
                 response = requests.post(
-                    self.url, data=json.dumps(finding), auth=(self.client_id, self.api_key)
+                    self.url, data=json.dumps(finding), auth=(client_id, api_key)
                 )
         else:
             raise ValueError("Missing credentials for client_id or api_key")
