503a4be7df72751e2924779f358fa1c5ff145e9b
Jody Brazil <jody.brazil@gmail.com>
2020-06-30 14:13:18 -0500

    move create insights into controller

--- eeauditor/controller.py
+++ eeauditor/controller.py
@@ -20,6 +20,7 @@ import sys
 import boto3
 import click
 
+from insights import create_sechub_insights
 from eeauditor import EEAuditor
 from processor.main import get_providers, process_findings
 
@@ -35,23 +36,45 @@ def run_auditor(auditor_name=None, check_name=None, delay=0, outputs=None, outpu
         outputs = ["sechub"]
     app = EEAuditor(name="AWS Auditor")
     app.load_plugins(plugin_name=auditor_name)
-    findings = list(app.run_checks(
-        requested_check_name=check_name, delay=delay))
-    result = process_findings(
-        findings=findings, outputs=outputs, output_file=output_file)
+    findings = list(app.run_checks(requested_check_name=check_name, delay=delay))
+    result = process_findings(findings=findings, outputs=outputs, output_file=output_file)
     print(f"Done.")
 
 
 @click.command()
-@click.option('-p', '--profile-name', default="", help='User profile to use')
-@click.option('-a', '--auditor-name', default="", help='Auditor to test defaulting to all auditors')
-@click.option('-c', '--check-name', default="", help='Check to test defaulting to all checks')
-@click.option('-d', '--delay', default=0, help='Delay between auditors defaulting to 0')
-@click.option('-o', '--outputs', multiple=True, default=(["sechub"]), show_default=True, help='Outputs for findings')
-@click.option('--output-file', default="output", show_default=True, help='File to output findings')
-@click.option('--list-options', is_flag=True, help='List output options')
-@click.option('--list-checks', is_flag=True, help='List all checks')
-def main(profile_name, auditor_name, check_name, delay, outputs, output_file, list_options, list_checks):
+@click.option("-p", "--profile-name", default="", help="User profile to use")
+@click.option(
+    "-a", "--auditor-name", default="", help="Auditor to test defaulting to all auditors"
+)
+@click.option("-c", "--check-name", default="", help="Check to test defaulting to all checks")
+@click.option("-d", "--delay", default=0, help="Delay between auditors defaulting to 0")
+@click.option(
+    "-o",
+    "--outputs",
+    multiple=True,
+    default=(["sechub"]),
+    show_default=True,
+    help="Outputs for findings",
+)
+@click.option("--output-file", default="output", show_default=True, help="File to output findings")
+@click.option("--list-options", is_flag=True, help="List output options")
+@click.option("--list-checks", is_flag=True, help="List all checks")
+@click.option(
+    "--create-insights",
+    is_flag=True,
+    help="Create SecurityHub insights for ElectricEye.  This only needs to be done once per SecurityHub instance",
+)
+def main(
+    profile_name,
+    auditor_name,
+    check_name,
+    delay,
+    outputs,
+    output_file,
+    list_options,
+    list_checks,
+    create_insights,
+):
     if list_options:
         print(get_providers())
         sys.exit(2)
@@ -62,6 +85,10 @@ def main(profile_name, auditor_name, check_name, delay, outputs, output_file, li
 
     if profile_name:
         boto3.setup_default_session(profile_name=profile_name)
+
+    if create_insights:
+        create_sechub_insights()
+        sys.exit(2)
 
     run_auditor(
         auditor_name=auditor_name,
