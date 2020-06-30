9b1cc4365dd6c19d80a1635c81d69bf29c8cd8e5
Jody Brazil <jody.brazil@gmail.com>
2020-06-30 11:58:12 -0500

    move ssm calls to init

--- eeauditor/processor/outputs/dops.py
+++ eeauditor/processor/outputs/dops.py
@@ -4,28 +4,29 @@ import os
 import requests
 from processor.outputs.output_base import ElectricEyeOutput
 
-ssm = boto3.client("ssm")
-
-dops_client_id_param = os.environ["DOPS_CLIENT_ID_PARAM"]
-dops_api_key_param = os.environ["DOPS_API_KEY_PARAM"]
-
-client_id_response = ssm.get_parameter(Name=dops_client_id_param, WithDecryption=True)
-api_key_response = ssm.get_parameter(Name=dops_api_key_param, WithDecryption=True)
-
-url = "https://collector.prod.disruptops.com/event"
-client_id = str(client_id_response["Parameter"]["Value"])
-api_key = str(api_key_response["Parameter"]["Value"])
-
 
 @ElectricEyeOutput
 class DopsProvider(object):
     __provider__ = "dops"
 
+    def __init__(self):
+        ssm = boto3.client("ssm")
+
+        dops_client_id_param = os.environ["DOPS_CLIENT_ID_PARAM"]
+        dops_api_key_param = os.environ["DOPS_API_KEY_PARAM"]
+
+        client_id_response = ssm.get_parameter(Name=dops_client_id_param, WithDecryption=True)
+        api_key_response = ssm.get_parameter(Name=dops_api_key_param, WithDecryption=True)
+
+        self.url = "https://collector.prod.disruptops.com/event"
+        self.client_id = str(client_id_response["Parameter"]["Value"])
+        self.api_key = str(api_key_response["Parameter"]["Value"])
+
     def write_findings(self, findings: list, **kwargs):
-        if client_id and api_key and url:
+        if self.client_id and self.api_key and self.url:
             for finding in findings:
                 response = requests.post(
-                    url, data=json.dumps(finding), auth=(client_id, api_key)
+                    self.url, data=json.dumps(finding), auth=(self.client_id, self.api_key)
                 )
         else:
             raise ValueError("Missing credentials for client_id or api_key")
