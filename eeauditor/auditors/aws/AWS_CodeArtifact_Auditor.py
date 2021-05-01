630f494dba48c2613ebf27a9e2e802f48a2d36eb
bleemb <bleemb@gmail.com>
2021-05-01 17:20:50 +1000

    domain policy auditor & test cases

--- eeauditor/auditors/aws/AWS_CodeArtifact_Auditor.py
+++ eeauditor/auditors/aws/AWS_CodeArtifact_Auditor.py
@@ -50,24 +50,25 @@ def codeartifact_repo_policy_check(
                 if statement["Effect"] == 'Allow':
                     if statement.get("Principal") == '*' or len([i for i in statement['Principal'].values() if f'arn:aws:iam::{awsAccountId}:root' in i]) > 0: 
                         if statement.get('Condition') == None: 
-                            # monitor for Update/Delete repo actions with no specified principal
+                            # monitor for Update/Delete repo actions with no principal listed
                             if "PublishPackageVersion" in statement.get('Action') or \
                                 "PutRepositoryPermissionsPolicy" in statement.get('Action') or \
                                 "UpdateRepository" in statement.get('Action') or \
+                                "DeleteRepositoryPermissionsPolicy" in statement.get('Action') or \
                                 "DisposePackageVersions" in statement.get('Action'):
-                                accessibility = "not_limited"
+                                    accessibility = "not_limited"
 
                             elif statement.get('Action') == "*" or statement.get('Action') == "codeartifact:*":
                                 accessibility = "not_limited"
 
         except codeartifact.exceptions.ResourceNotFoundException:
             # If no policy is applied, access is repo owner only.
-            accessibility = "limited"
+            accessibility = "not_limited"
 
         if accessibility == "limited":
             finding = {
                 "SchemaVersion": "2018-10-08",
-                "Id": repoArn + "/codeartifact_repo_iam_check",
+                "Id": repoArn + "/codeartifact_repo_policy_check",
                 "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
                 "GeneratorId": repoArn,
                 "AwsAccountId": awsAccountId,
@@ -115,10 +116,10 @@ def codeartifact_repo_policy_check(
             }
             yield finding
         
-        elif accessibility == "limited":
+        else:
             finding = {
                 "SchemaVersion": "2018-10-08",
-                "Id": repoArn + "/codeartifact_repo_iam_check",
+                "Id": repoArn + "/codeartifact_repo_policy_check",
                 "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
                 "GeneratorId": repoArn,
                 "AwsAccountId": awsAccountId,
@@ -146,6 +147,153 @@ def codeartifact_repo_policy_check(
                         "Details": {"Other": {
                             "name": repositoryName,
                             "domain": domainName}},
+                    }
+                ],
+                "Compliance": {
+                    "Status": "FAILED",
+                    "RelatedRequirements": [
+                            "NIST CSF PR.AC-4",
+                            "NIST CSF PR.DS-5",
+                            "NIST CSF PR.PT-3",
+                            "NIST SP 800-53 AC-1"
+                            "NIST SP 800-53 AC-3"
+                            "NIST SP 800-53 AC-17"
+                            "NIST SP 800-53 AC-22"
+                            "ISO 27001:2013 A.13.1.2"
+                    ],
+                },
+                "Workflow": {"Status": "NEW"},
+                "RecordState": "ACTIVE",
+            }
+            yield finding
+
+
+@registry.register_check("codeartifact")
+def codeartifact_domain_policy_check(
+    cache: dict, awsAccountId: str, awsRegion: str, awsPartition: str
+) -> dict:
+    response = codeartifact.list_domains()
+    iso8601Time = datetime.datetime.now(datetime.timezone.utc).isoformat()
+    
+    for domain in response["domains"]:
+        domainName = domain['name']
+        domainOwner = domain['owner']
+        status = domain['status']
+        domainArn = f"arn:aws:codeartifact:{awsRegion}:{domainOwner}:domain/{domainName}"
+        try: 
+            domain_policy = codeartifact.get_domain_permissions_policy(
+                domain=domainName,
+                domainOwner=domainOwner
+            )
+            policy = json.loads(domain_policy['policy']['document'])
+            accessibility = "limited"
+
+            for statement in policy["Statement"]:
+                if statement["Effect"] == 'Allow':
+                    if statement.get("Principal") == '*' or len([i for i in statement['Principal'].values() if f'arn:aws:iam::{awsAccountId}:root' in i]) > 0: 
+                        if statement.get('Condition') == None: 
+                            # monitor for Update/Delete actions with no principal listed
+                            if "PublishPackageVersion" in statement.get('Action') or \
+                                "PutRepositoryPermissionsPolicy" in statement.get('Action') or \
+                                "UpdateRepository" in statement.get('Action') or \
+                                "DisposePackageVersions" in statement.get('Action') or \
+                                "CreateDomain" in statement.get('Action') or \
+                                "CreateRepository" in statement.get('Action') or \
+                                "DeleteDomain" in statement.get('Action') or \
+                                "DeleteDomainPermissionsPolicy" in statement.get('Action') or \
+                                "DeleteRepositoryPermissionsPolicy" in statement.get('Action') or \
+                                "PutDomainPermissionsPolicy" in statement.get('Action'):
+                                    accessibility = "not_limited"
+
+                            elif statement.get('Action') == "*" or statement.get('Action') == "codeartifact:*":
+                                accessibility = "not_limited"
+
+        except codeartifact.exceptions.ResourceNotFoundException:
+            # If no policy is applied, domain (and underlying repos) are owner only.
+            accessibility = "not_limited"
+
+        if accessibility == "limited":
+            finding = {
+                "SchemaVersion": "2018-10-08",
+                "Id": domainArn + "/codeartifact_domain_policy_check",
+                "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+                "GeneratorId": domainArn,
+                "AwsAccountId": awsAccountId,
+                "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
+                "FirstObservedAt": iso8601Time,
+                "CreatedAt": iso8601Time,
+                "UpdatedAt": iso8601Time,
+                "Severity": {"Label": "INFORMATIONAL"},
+                "Confidence": 99,
+                "Title": "[CodeArtifact.2] CodeArtifact domains should have a resource policy with least privilege applied",
+                "Description": f"CodeArtifact domain {domainName} has a resource policy with least privilege applied",
+                "Remediation": {
+                    "Recommendation": {
+                        "Text": "CodeArtifact domains should use resource policies to further protect repositories from unauthorized access.  See the CodeArtifact docs for more details",
+                        "Url": "https://docs.aws.amazon.com/codeartifact/latest/ug/domain-policies.html",
+                    }
+                },
+                "ProductFields": {"Product Name": "ElectricEye"},
+                "Resources": [
+                    {
+                        "Type": "AwsCodeArtifactDomain",
+                        "Id": domainArn,
+                        "Partition": awsPartition,
+                        "Region": awsRegion,
+                        "Details": {"Other": {
+                            "name": domainName,
+                            "status": status}},
+                    }
+                ],
+                "Compliance": {
+                    "Status": "PASSED",
+                    "RelatedRequirements": [
+                            "NIST CSF PR.AC-4",
+                            "NIST CSF PR.DS-5",
+                            "NIST CSF PR.PT-3",
+                            "NIST SP 800-53 AC-1"
+                            "NIST SP 800-53 AC-3"
+                            "NIST SP 800-53 AC-17"
+                            "NIST SP 800-53 AC-22"
+                            "ISO 27001:2013 A.13.1.2"
+                    ],
+                },
+                "Workflow": {"Status": "RESOLVED"},
+                "RecordState": "ARCHIVED",
+            }
+            yield finding
+        
+        else:
+            finding = {
+                "SchemaVersion": "2018-10-08",
+                "Id": domainArn + "/codeartifact_domain_policy_check",
+                "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+                "GeneratorId": domainArn,
+                "AwsAccountId": awsAccountId,
+                "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
+                "FirstObservedAt": iso8601Time,
+                "CreatedAt": iso8601Time,
+                "UpdatedAt": iso8601Time,
+                "Severity": {"Label": "LOW"},
+                "Confidence": 99,
+                "Title": "[CodeArtifact.2] CodeArtifact domains should have a resource policy with least privilege applied",
+                "Description": f"CodeArtifact domain {domainName} does not have a resource policy with least privilege applied",
+                "Remediation": {
+                    "Recommendation": {
+                        "Text": "CodeArtifact domains should use resource policies to further protect repositories from unauthorized access.  See the CodeArtifact docs for more details",
+                        "Url": "https://docs.aws.amazon.com/codeartifact/latest/ug/domain-policies.html",
+                    }
+                },
+                "ProductFields": {"Product Name": "ElectricEye"},
+                "Resources": [
+                    {
+                        "Type": "AwsCodeArtifactDomain",
+                        "Id": domainArn,
+                        "Partition": awsPartition,
+                        "Region": awsRegion,
+                        "Details": {"Other": {
+                            "name": domainName,
+                            "status": status}},
                     }
                 ],
                 "Compliance": {
--- eeauditor/tests/test_AWS_CodeArtifact_Auditor.py
+++ eeauditor/tests/test_AWS_CodeArtifact_Auditor.py
@@ -8,6 +8,7 @@ from botocore.stub import Stubber, ANY
 from . import context
 from auditors.aws.AWS_CodeArtifact_Auditor import (
     codeartifact_repo_policy_check,
+    codeartifact_domain_policy_check,
     codeartifact
 )
 
@@ -24,7 +25,17 @@ list_repositories = {
     ]
 }
 
-get_repository_permissions_policy_root_list_star = {
+list_domains = {
+    'domains': [
+        {'name': 'eg-domain', 
+        'owner': '111122223333', 
+        'status': 'Active', 
+        'encryptionKey': 'arn:aws:kms:ap-southeast-2:111122223333:key/abcdef-123456'
+        }
+    ]
+}
+
+get_repository_permissions_policy_root_list_star = {    
     "policy": {
         "resourceArn": "arn:aws:codeartifact:us-west-2:111122223333:repository/my-domain/npm-store",
         'revision': '1.0',
@@ -49,7 +60,7 @@ get_repository_permissions_policy_root_star = {
             [{"Sid":"__owner_statement", \
             "Effect":"Allow", \
             "Principal": \
-            {"AWS":"arn:aws:iam::012345678901:root"}, \
+            {"AWS":"arn:aws:iam::111122223333:root"}, \
             "Action":"codeartifact:*", \
             "Resource":"arn:aws:codeartifact:us-west-2:111122223333:repository/my-domain/npm-store"}]}'
     }
@@ -99,6 +110,65 @@ get_repository_permissions_policy_star_star_condition = {
                 "aws:sourceVpce":"vpce-1a2b3c4d"}}}]}'}
     }
 
+get_domain_permissions_policy_star_delete = {
+    "policy": {
+        "resourceArn": "arn:aws:codeartifact:us-west-2:111122223333:domain/eg-domain",
+        'revision': '1.0',
+        "document": '{"Version":"2008-10-17","Id":"__default_policy_ID", \
+        "Statement": \
+            [{"Sid":"__owner_statement", \
+            "Effect":"Allow", \
+            "Principal": "*", \
+            "Action":"codeartifact:DeleteDomainPermissionsPolicy", \
+            "Resource":"*"}]}'
+    }
+}
+
+
+get_domain_permissions_policy_star_list = {
+    "policy": {
+        "resourceArn": "arn:aws:codeartifact:us-west-2:111122223333:domain/eg-domain",
+        'revision': '1.0',
+        "document": '{"Version":"2008-10-17","Id":"__default_policy_ID", \
+        "Statement": \
+            [{"Sid":"__owner_statement", \
+            "Effect":"Allow", \
+            "Principal": "*", \
+            "Action":"codeartifact:List*", \
+            "Resource":"*"}]}'
+    }
+}
+
+get_domain_permissions_policy_star_star_condition = {
+    "policy": {
+    "resourceArn": "arn:aws:codeartifact:us-west-2:111122223333:domain/eg-domain", 
+    'revision': '1.0',
+    "document": '{"Version":"2008-10-17","Id":"__default_policy_ID", \
+    "Statement": \
+        [{"Sid":"__owner_statement", \
+        "Effect":"Allow", \
+        "Principal": "*", \
+        "Action":"*", \
+        "Resource":"*", \
+        "Condition":{ \
+            "StringEquals":{ \
+                "aws:sourceVpce":"vpce-1a2b3c4d"}}}]}'}
+    }
+
+get_domain_permissions_policy_root_star = {    
+    "policy": {
+        "resourceArn": "arn:aws:codeartifact:us-west-2:111122223333:domain/eg-domain",
+        'revision': '1.0',
+        "document": '{"Version":"2008-10-17","Id":"__default_policy_ID", \
+        "Statement": \
+            [{"Sid":"__owner_statement", \
+            "Effect":"Allow", \
+            "Principal": \
+            {"AWS":"arn:aws:iam::111122223333:root"}, \
+            "Action":"codeartifact:*", \
+            "Resource":"arn:aws:codeartifact:us-west-2:111122223333:domain/eg-domain"}]}'
+    }
+}
 
 @pytest.fixture(scope="function")
 def codeartifact_stubber():
@@ -112,7 +182,7 @@ def test_policy_star_list(codeartifact_stubber):
     codeartifact_stubber.add_response("list_repositories", list_repositories)
     codeartifact_stubber.add_response("get_repository_permissions_policy", get_repository_permissions_policy_root_list_star)
     results = codeartifact_repo_policy_check(
-        cache={}, awsAccountId="012345678901", awsRegion="us-east-1", awsPartition="aws"
+        cache={}, awsAccountId="111122223333", awsRegion="us-east-1", awsPartition="aws"
     )
     for result in results:
         if "npm-store" in result["Id"]:
@@ -126,7 +196,7 @@ def test_policy_root_user(codeartifact_stubber):
     codeartifact_stubber.add_response("list_repositories", list_repositories)
     codeartifact_stubber.add_response("get_repository_permissions_policy", get_repository_permissions_policy_root_star)
     results = codeartifact_repo_policy_check(
-        cache={}, awsAccountId="012345678901", awsRegion="us-east-1", awsPartition="aws"
+        cache={}, awsAccountId="111122223333", awsRegion="us-east-1", awsPartition="aws"
     )
     for result in results:
         if "npm-store" in result["Id"]:
@@ -140,7 +210,7 @@ def test_policy_star_update(codeartifact_stubber):
     codeartifact_stubber.add_response("list_repositories", list_repositories)
     codeartifact_stubber.add_response("get_repository_permissions_policy", get_repository_permissions_policy_star_update)
     results = codeartifact_repo_policy_check(
-        cache={}, awsAccountId="012345678901", awsRegion="us-east-1", awsPartition="aws"
+        cache={}, awsAccountId="111122223333", awsRegion="us-east-1", awsPartition="aws"
     )
     for result in results:
         if "npm-store" in result["Id"]:
@@ -154,7 +224,7 @@ def test_policy_star_star(codeartifact_stubber):
     codeartifact_stubber.add_response("list_repositories", list_repositories)
     codeartifact_stubber.add_response("get_repository_permissions_policy", get_repository_permissions_policy_star_star)
     results = codeartifact_repo_policy_check(
-        cache={}, awsAccountId="012345678901", awsRegion="us-east-1", awsPartition="aws"
+        cache={}, awsAccountId="111122223333", awsRegion="us-east-1", awsPartition="aws"
     )
     for result in results:
         if "npm-store" in result["Id"]:
@@ -168,7 +238,7 @@ def test_policy_star_star_condition(codeartifact_stubber):
     codeartifact_stubber.add_response("list_repositories", list_repositories)
     codeartifact_stubber.add_response("get_repository_permissions_policy", get_repository_permissions_policy_star_star_condition)
     results = codeartifact_repo_policy_check(
-        cache={}, awsAccountId="012345678901", awsRegion="us-east-1", awsPartition="aws"
+        cache={}, awsAccountId="111122223333", awsRegion="us-east-1", awsPartition="aws"
     )
     for result in results:
         if "npm-store" in result["Id"]:
@@ -182,11 +252,81 @@ def test_policy_no_policy(codeartifact_stubber):
     codeartifact_stubber.add_response("list_repositories", list_repositories)
     codeartifact_stubber.add_client_error("get_repository_permissions_policy", "ResourceNotFoundException")
     results = codeartifact_repo_policy_check(
-        cache={}, awsAccountId="012345678901", awsRegion="us-east-1", awsPartition="aws"
+        cache={}, awsAccountId="111122223333", awsRegion="us-east-1", awsPartition="aws"
     )
     for result in results:
         if "npm-store" in result["Id"]:
+            assert result["RecordState"] == "ACTIVE"
+        else:
+            assert False
+    codeartifact_stubber.assert_no_pending_responses()
+
+
+def test_domain_no_policy(codeartifact_stubber):
+    codeartifact_stubber.add_response("list_domains", list_domains)
+    codeartifact_stubber.add_client_error("get_domain_permissions_policy", "ResourceNotFoundException")
+    results = codeartifact_domain_policy_check(
+        cache={}, awsAccountId="111122223333", awsRegion="us-east-1", awsPartition="aws"
+    )
+    for result in results:
+        if "eg-domain" in result["Id"]:
+            assert result["RecordState"] == "ACTIVE"
+        else:
+            assert False
+    codeartifact_stubber.assert_no_pending_responses()
+
+
+def test_domain_star_delete(codeartifact_stubber):
+    codeartifact_stubber.add_response("list_domains", list_domains)
+    codeartifact_stubber.add_response("get_domain_permissions_policy", get_domain_permissions_policy_star_delete)
+    results = codeartifact_domain_policy_check(
+        cache={}, awsAccountId="111122223333", awsRegion="us-east-1", awsPartition="aws"
+    )
+    for result in results:
+        if "eg-domain" in result["Id"]:
+            assert result["RecordState"] == "ACTIVE"
+        else:
+            assert False
+    codeartifact_stubber.assert_no_pending_responses()
+
+
+def test_domain_star_list(codeartifact_stubber):
+    codeartifact_stubber.add_response("list_domains", list_domains)
+    codeartifact_stubber.add_response("get_domain_permissions_policy", get_domain_permissions_policy_star_list)
+    results = codeartifact_domain_policy_check(
+        cache={}, awsAccountId="111122223333", awsRegion="us-east-1", awsPartition="aws"
+    )
+    for result in results:
+        if "eg-domain" in result["Id"]:
             assert result["RecordState"] == "ARCHIVED"
         else:
             assert False
     codeartifact_stubber.assert_no_pending_responses()
+
+def test_domain_star_delete(codeartifact_stubber):
+    codeartifact_stubber.add_response("list_domains", list_domains)
+    codeartifact_stubber.add_response("get_domain_permissions_policy", get_domain_permissions_policy_star_star_condition)
+    results = codeartifact_domain_policy_check(
+        cache={}, awsAccountId="111122223333", awsRegion="us-east-1", awsPartition="aws"
+    )
+    for result in results:
+        if "eg-domain" in result["Id"]:
+            assert result["RecordState"] == "ARCHIVED"
+        else:
+            assert False
+    codeartifact_stubber.assert_no_pending_responses()
+
+
+def test_domain_root_star(codeartifact_stubber):
+    codeartifact_stubber.add_response("list_domains", list_domains)
+    codeartifact_stubber.add_response("get_domain_permissions_policy", get_domain_permissions_policy_root_star)
+    results = codeartifact_domain_policy_check(
+        cache={}, awsAccountId="111122223333", awsRegion="us-east-1", awsPartition="aws"
+    )
+    for result in results:
+        if "eg-domain" in result["Id"]:
+            assert result["RecordState"] == "ACTIVE"
+        else:
+            assert False
+    codeartifact_stubber.assert_no_pending_responses()
+
