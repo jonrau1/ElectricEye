76748f1066624487bd33d966dc928deb01b32c7b
Jonathan Rau <46727149+jonrau1@users.noreply.github.com>
2021-09-22 23:22:23 -0400

    Update postgresql.py

--- eeauditor/processor/outputs/postgresql.py
+++ eeauditor/processor/outputs/postgresql.py
@@ -66,10 +66,6 @@ class PostgresProvider(object):
                 )
                 cursor = engine.cursor()
                 
-                # drop previously existing tables
-                cursor.execute("""DROP TABLE IF EXISTS electriceye_findings""")
-                engine.commit()
-                
                 # Create a new table for the ElectricEye findings. ID will be the Primary Key, all other elements will be parsed as text
                 cursor.execute("""CREATE TABLE IF NOT EXISTS electriceye_findings( schemaversion TEXT, id TEXT PRIMARY KEY, awsaccountid TEXT, productarn TEXT, generatorid TEXT, types TEXT, firstobservedat TEXT, createdat TEXT, updatedat TEXT, severitylabel TEXT, confidence TEXT, title TEXT, description TEXT, remediationtext TEXT, remediationurl TEXT, resourcetype TEXT, resourceid TEXT, resourceregion TEXT, resourcepartition TEXT, compliancestatus TEXT, workflowstatus TEXT, recordstate TEXT);""")
 
