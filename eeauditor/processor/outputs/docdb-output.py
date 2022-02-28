f309db0075a1c16fdcdc8b9b9ba3c079efafa68e
jonrau1 <46727149+jonrau1@users.noreply.github.com>
2022-02-28 12:02:56 -0500

    Docdb missing `pymongo` dep

--- eeauditor/processor/outputs/docdb-output.py
+++ eeauditor/processor/outputs/docdb-output.py
@@ -17,7 +17,6 @@
 #KIND, either express or implied.  See the License for the
 #specific language governing permissions and limitations
 #under the License.
-import sys
 import os
 import boto3
 import requests
