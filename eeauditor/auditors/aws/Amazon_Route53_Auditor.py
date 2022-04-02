2571da276520e36dbca350d645938a3ed4d33832
Jonathan Rau <46727149+jonrau1@users.noreply.github.com>
2022-04-02 17:30:21 -0400

    Add Traffic policy rt53 check

--- eeauditor/auditors/aws/Amazon_Route53_Auditor.py
+++ eeauditor/auditors/aws/Amazon_Route53_Auditor.py
@@ -124,7 +124,7 @@ def route53_hosted_zone_query_logging_check(cache: dict, awsAccountId: str, awsR
                 "Severity": {"Label": "INFORMATIONAL"},
                 "Confidence": 99,
                 "Title": "[Route53.1] Route53 Hosted Zones should have query logging configured",
-                "Description": f"Route53 Hosted Zone {hzId} named {hzName} does not have query logging configured. Query logs contain only the queries that DNS resolvers forward to Route 53. If a DNS resolver has already cached the response to a query (such as the IP address for a load balancer for example.com), the resolver will continue to return the cached response without forwarding the query to Route 53 until the TTL for the corresponding record expires. Refer to the remediation instructions to remediate this behavior.",
+                "Description": f"Route53 Hosted Zone {hzId} named {hzName} has query logging configured.",
                 "Remediation": {
                     "Recommendation": {
                         "Text": "For more information on Route53 query logging and how to configure it refer to the Public DNS query logging section of the Amazon Route53 Developer Guide.",
@@ -160,6 +160,134 @@ def route53_hosted_zone_query_logging_check(cache: dict, awsAccountId: str, awsR
                         "AICPA TSC CC7.2",
                         "ISO 27001:2013 A.12.4.1",
                         "ISO 27001:2013 A.16.1.7"
+                    ]
+                },
+                "Workflow": {"Status": "RESOLVED"},
+                "RecordState": "ARCHIVED"
+            }
+            yield finding
+
+@registry.register_check("route53")
+def route53_hosted_zone_traffic_policy_check(cache: dict, awsAccountId: str, awsRegion: str, awsPartition: str) -> dict:
+    """[Route53.2] Route53 Hosted Zones should have traffic policies configured"""
+    # ISO Time
+    iso8601Time = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    for zone in get_hosted_zones(cache=cache):
+        hzId = zone["Id"]
+        hzName = zone["Name"]
+        hzArn = f"arn:aws:route53:::hostedzone/{hzName}"
+        hzType = zone["Config"]["PrivateZone"]
+        # check specific metadata
+        r = route53.list_traffic_policy_instances_by_hosted_zone(HostedZoneId=hzId)["TrafficPolicyInstances"]
+        # empty lists return mean there is not a configuration
+        if not r:
+            # this is a failing check
+            finding = {
+                "SchemaVersion": "2018-10-08",
+                "Id": f"{hzArn}/route53-traffic-policy-check",
+                "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+                "GeneratorId": hzArn,
+                "AwsAccountId": awsAccountId,
+                "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
+                "FirstObservedAt": iso8601Time,
+                "CreatedAt": iso8601Time,
+                "UpdatedAt": iso8601Time,
+                "Severity": {"Label": "LOW"},
+                "Confidence": 99,
+                "Title": "[Route53.2] Route53 Hosted Zones should have traffic policies configured",
+                "Description": f"Route53 Hosted Zone {hzId} named {hzName} does not have traffic policies configured. Traffic policies greatly simplifies the process of creating and maintaining records in large and complex traffic configurations, such as creating a configuration in which latency alias records reference weighted records, and the weighted records reference your resources in multiple AWS Regions. Refer to the remediation instructions to remediate this behavior.",
+                "Remediation": {
+                    "Recommendation": {
+                        "Text": "For more information on Route53 traffic policy and how to configure it refer to the Using traffic flow to route DNS traffic section of the Amazon Route53 Developer Guide.",
+                        "Url": "https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/traffic-flow.html",
+                    }
+                },
+                "ProductFields": {"Product Name": "ElectricEye"},
+                "Resources": [
+                    {
+                        "Type": "AwsRoute53HostedZone",
+                        "Id": hzArn,
+                        "Partition": awsPartition,
+                        "Region": awsRegion,
+                        "Details": {
+                            "Other": {
+                                "Id": hzId,
+                                "Name": hzName,
+                                "PrivateZone": hzType
+                            }
+                        }
+                    }
+                ],
+                "Compliance": {
+                    "Status": "FAILED",
+                    "RelatedRequirements": [
+                        "NIST CSF PR.AC-5",
+                        "NIST SP 800-53 AC-4",
+                        "NIST SP 800-53 AC-10",
+                        "NIST SP 800-53 SC-7",
+                        "AICPA TSC CC6.1",
+                        "ISO 27001:2013 A.13.1.1",
+                        "ISO 27001:2013 A.13.1.3",
+                        "ISO 27001:2013 A.13.2.1",
+                        "ISO 27001:2013 A.14.1.2",
+                        "ISO 27001:2013 A.14.1.3"
+                    ]
+                },
+                "Workflow": {"Status": "NEW"},
+                "RecordState": "ACTIVE"
+            }
+            yield finding
+        else:
+            # this is a passing check
+            finding = {
+                "SchemaVersion": "2018-10-08",
+                "Id": f"{hzArn}/route53-traffic-policy-check",
+                "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+                "GeneratorId": hzArn,
+                "AwsAccountId": awsAccountId,
+                "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
+                "FirstObservedAt": iso8601Time,
+                "CreatedAt": iso8601Time,
+                "UpdatedAt": iso8601Time,
+                "Severity": {"Label": "INFORMATIONAL"},
+                "Confidence": 99,
+                "Title": "[Route53.2] Route53 Hosted Zones should have traffic policies configured",
+                "Description": f"Route53 Hosted Zone {hzId} named {hzName} has traffic policies configured.",
+                "Remediation": {
+                    "Recommendation": {
+                        "Text": "For more information on Route53 traffic policy and how to configure it refer to the Using traffic flow to route DNS traffic section of the Amazon Route53 Developer Guide.",
+                        "Url": "https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/traffic-flow.html",
+                    }
+                },
+                "ProductFields": {"Product Name": "ElectricEye"},
+                "Resources": [
+                    {
+                        "Type": "AwsRoute53HostedZone",
+                        "Id": hzArn,
+                        "Partition": awsPartition,
+                        "Region": awsRegion,
+                        "Details": {
+                            "Other": {
+                                "Id": hzId,
+                                "Name": hzName,
+                                "PrivateZone": hzType
+                            }
+                        }
+                    }
+                ],
+                "Compliance": {
+                    "Status": "PASSED",
+                    "RelatedRequirements": [
+                        "NIST CSF PR.AC-5",
+                        "NIST SP 800-53 AC-4",
+                        "NIST SP 800-53 AC-10",
+                        "NIST SP 800-53 SC-7",
+                        "AICPA TSC CC6.1",
+                        "ISO 27001:2013 A.13.1.1",
+                        "ISO 27001:2013 A.13.1.3",
+                        "ISO 27001:2013 A.13.2.1",
+                        "ISO 27001:2013 A.14.1.2",
+                        "ISO 27001:2013 A.14.1.3"
                     ]
                 },
                 "Workflow": {"Status": "RESOLVED"},
