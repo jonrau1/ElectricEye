7d96c6f385fea0a9774980422dafc5a022fa35e3
Jonathan Rau <46727149+jonrau1@users.noreply.github.com>
2022-08-03 18:14:10 -0400

    Add client-side override athena check

--- eeauditor/auditors/aws/Amazon_Athena_Auditor.py
+++ eeauditor/auditors/aws/Amazon_Athena_Auditor.py
@@ -226,8 +226,205 @@ def athena_workgroup_encryption_check(cache: dict, awsAccountId: str, awsRegion:
         # this is a cautionary function in case encryption options are ever expanded
         else:
             print(f"Athena workgroup {workgroupName} has an encryption option of {encryptionOption} which was not accounted for...")
+            continue
 
-"""[Athena.2] Athena workgroups that enforce query result encryption should be configured to override client-side settings"""
+@registry.register_check("athena")
+def athena_encrypted_workgroup_client_override_check(cache: dict, awsAccountId: str, awsRegion: str, awsPartition: str) -> dict:
+    """[Athena.2] Athena workgroups that enforce query result encryption should be configured to override client-side settings"""
+    # ISO time
+    iso8601Time = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    # loop work groups from cache
+    for wgroup in list_work_groups(cache)["WorkGroups"]:
+        workgroupName = wgroup["Name"]
+        workgroupArn = f"arn:{awsPartition}:athena:{awsRegion}:{awsAccountId}:workgroup/{workgroupName}"
+        # get specific details from workgroup
+        wginfo = athena.get_work_group(WorkGroup=workgroupName)["WorkGroup"]
+        # determine if there is an encryption - this dict will be missing if it is not
+        try:
+            encryptionOption = wginfo["Configuration"]["ResultConfiguration"]["EncryptionConfiguration"]["EncryptionOption"]
+        except KeyError:
+            encryptionOption = "NO_ENCRYPTION"
+        # next check the workgroup client-side override configuration, this *shouldn't* be missing, ever...but will plan for it
+        try:
+            overrideConfig = wginfo["Configuration"]["EnforceWorkGroupConfiguration"]
+        except KeyError:
+            overrideConfig = False
+        # logic ladder - no encryption = fail, encryption + no-override = fail, encryption + override = pass, anything else = skip
+        # this is a failing check (high - to match lacking encryption - so you double fail)
+        if encryptionOption == "NO_ENCRYPTION":
+            finding = {
+                "SchemaVersion": "2018-10-08",
+                "Id": f"{workgroupArn}/athena-encrypted-workgroup-client-override-check",
+                "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+                "GeneratorId": workgroupArn,
+                "AwsAccountId": awsAccountId,
+                "Types": [
+                    "Software and Configuration Checks/AWS Security Best Practices",
+                    "Effects/Data Exposure"
+                ],
+                "FirstObservedAt": iso8601Time,
+                "CreatedAt": iso8601Time,
+                "UpdatedAt": iso8601Time,
+                "Severity": {"Label": "HIGH"},
+                "Confidence": 99,
+                "Title": "[Athena.2] Athena workgroups that enforce query result encryption should be configured to override client-side settings",
+                "Description": f"Athena workgroup {workgroupName} does not enforce query result encryption at all. You set up query result encryption using the Athena console or when using JDBC or ODBC. Workgroups allow you to enforce the encryption of query results.  When you create or edit a workgroup and select the Override client-side settings field, then all queries that run in this workgroup use the workgroup encryption and query results location settings. Refer to the remediation instructions to remediate this behavior.",
+                "Remediation": {
+                    "Recommendation": {
+                        "Text": "For more information on Athena query result encryption refer to the Encrypting Athena query results stored in Amazon S3 section in the Amazon Athena User Guide.",
+                        "Url": "https://docs.aws.amazon.com/athena/latest/ug/encrypting-query-results-stored-in-s3.html"
+                    }
+                },
+                "ProductFields": {"Product Name": "ElectricEye"},
+                "Resources": [
+                    {
+                        "Type": "AwsAthenaWorkGroup",
+                        "Id": workgroupArn,
+                        "Partition": awsPartition,
+                        "Region": awsRegion,
+                        "Details": {
+                            "Other": {
+                                "Name": workgroupName,
+                                "State": wginfo["State"],
+                                "Description": wginfo["Description"],
+                                "CreationTime": str(wginfo["CreationTime"])
+                            }
+                        }
+                    }
+                ],
+                "Compliance": {
+                    "Status": "FAILED",
+                    "RelatedRequirements": [
+                        "NIST CSF PR.DS-1",
+                        "NIST SP 800-53 MP-8",
+                        "NIST SP 800-53 SC-12",
+                        "NIST SP 800-53 SC-28",
+                        "AICPA TSC CC6.1",
+                        "ISO 27001:2013 A.8.2.3"
+                    ]
+                },
+                "Workflow": {"Status": "NEW"},
+                "RecordState": "ACTIVE"
+            }
+            yield finding
+        # this is a failing check (medium)
+        elif (encryptionOption != "NO_ENCRYPTION" and overrideConfig == False):
+            finding = {
+                "SchemaVersion": "2018-10-08",
+                "Id": f"{workgroupArn}/athena-encrypted-workgroup-client-override-check",
+                "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+                "GeneratorId": workgroupArn,
+                "AwsAccountId": awsAccountId,
+                "Types": [
+                    "Software and Configuration Checks/AWS Security Best Practices",
+                    "Effects/Data Exposure"
+                ],
+                "FirstObservedAt": iso8601Time,
+                "CreatedAt": iso8601Time,
+                "UpdatedAt": iso8601Time,
+                "Severity": {"Label": "MEDIUM"},
+                "Confidence": 99,
+                "Title": "[Athena.2] Athena workgroups that enforce query result encryption should be configured to override client-side settings",
+                "Description": f"Athena workgroup {workgroupName} enforces query result encryption but does not override client-side settings. When you create or edit a workgroup and select the Override client-side settings field, then all queries that run in this workgroup use the workgroup encryption and query results location settings. Refer to the remediation instructions to remediate this behavior.",
+                "Remediation": {
+                    "Recommendation": {
+                        "Text": "For more information on Athena query result encryption enforcement via client-side overrides refer to the Workgroup settings override client-side settings section in the Amazon Athena User Guide.",
+                        "Url": "https://docs.aws.amazon.com/athena/latest/ug/workgroups-settings-override.html"
+                    }
+                },
+                "ProductFields": {"Product Name": "ElectricEye"},
+                "Resources": [
+                    {
+                        "Type": "AwsAthenaWorkGroup",
+                        "Id": workgroupArn,
+                        "Partition": awsPartition,
+                        "Region": awsRegion,
+                        "Details": {
+                            "Other": {
+                                "Name": workgroupName,
+                                "State": wginfo["State"],
+                                "Description": wginfo["Description"],
+                                "CreationTime": str(wginfo["CreationTime"])
+                            }
+                        }
+                    }
+                ],
+                "Compliance": {
+                    "Status": "FAILED",
+                    "RelatedRequirements": [
+                        "NIST CSF PR.DS-1",
+                        "NIST SP 800-53 MP-8",
+                        "NIST SP 800-53 SC-12",
+                        "NIST SP 800-53 SC-28",
+                        "AICPA TSC CC6.1",
+                        "ISO 27001:2013 A.8.2.3"
+                    ]
+                },
+                "Workflow": {"Status": "NEW"},
+                "RecordState": "ACTIVE"
+            }
+            yield finding
+        # this is a passing check
+        elif (encryptionOption != "NO_ENCRYPTION" and overrideConfig == True):
+            finding = {
+                "SchemaVersion": "2018-10-08",
+                "Id": f"{workgroupArn}/athena-encrypted-workgroup-client-override-check",
+                "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+                "GeneratorId": workgroupArn,
+                "AwsAccountId": awsAccountId,
+                "Types": [
+                    "Software and Configuration Checks/AWS Security Best Practices",
+                    "Effects/Data Exposure"
+                ],
+                "FirstObservedAt": iso8601Time,
+                "CreatedAt": iso8601Time,
+                "UpdatedAt": iso8601Time,
+                "Severity": {"Label": "INFORMATIONAL"},
+                "Confidence": 99,
+                "Title": "[Athena.2] Athena workgroups that enforce query result encryption should be configured to override client-side settings",
+                "Description": f"Athena workgroup {workgroupName} enforces query result encryption and also overrides client-side settings.",
+                "Remediation": {
+                    "Recommendation": {
+                        "Text": "For more information on Athena query result encryption enforcement via client-side overrides refer to the Workgroup settings override client-side settings section in the Amazon Athena User Guide.",
+                        "Url": "https://docs.aws.amazon.com/athena/latest/ug/workgroups-settings-override.html"
+                    }
+                },
+                "ProductFields": {"Product Name": "ElectricEye"},
+                "Resources": [
+                    {
+                        "Type": "AwsAthenaWorkGroup",
+                        "Id": workgroupArn,
+                        "Partition": awsPartition,
+                        "Region": awsRegion,
+                        "Details": {
+                            "Other": {
+                                "Name": workgroupName,
+                                "State": wginfo["State"],
+                                "Description": wginfo["Description"],
+                                "CreationTime": str(wginfo["CreationTime"])
+                            }
+                        }
+                    }
+                ],
+                "Compliance": {
+                    "Status": "PASSED",
+                    "RelatedRequirements": [
+                        "NIST CSF PR.DS-1",
+                        "NIST SP 800-53 MP-8",
+                        "NIST SP 800-53 SC-12",
+                        "NIST SP 800-53 SC-28",
+                        "AICPA TSC CC6.1",
+                        "ISO 27001:2013 A.8.2.3"
+                    ]
+                },
+                "Workflow": {"Status": "RESOLVED"},
+                "RecordState": "ARCHIVED"
+            }
+            yield finding
+        # this is a cautionary function in case something stupid happens
+        else:
+            print(f"Athena workgroup {workgroupName} has an encryption option or 'EnforceWorkGroupConfiguration' which was not accounted for...")
+            continue
 
 """[Athena.3] Athena workgroups should be configured to publish metrics"""
 
