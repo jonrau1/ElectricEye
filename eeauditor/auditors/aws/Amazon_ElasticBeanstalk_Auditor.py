970b2ca5ebf30883efe8252072c1b1b670c2f0cb
jonrau1 <46727149+jonrau1@users.noreply.github.com>
2022-08-03 10:40:39 -0400

    Stage Ebstalk IMDS check

--- eeauditor/auditors/aws/Amazon_ElasticBeanstalk_Auditor.py
+++ eeauditor/auditors/aws/Amazon_ElasticBeanstalk_Auditor.py
@@ -39,10 +39,11 @@ def describe_environments(cache):
 
 @registry.register_check("elasticbeanstalk")
 def elasticbeanstalk_imdsv1_disabled_check(cache: dict, awsAccountId: str, awsRegion: str, awsPartition: str) -> dict:
-    """[ElasticBeanstalk.1] Elastic Beanstalk applications should disable IMDSv1"""
+    """[ElasticBeanstalk.1] Elastic Beanstalk environments should disable IMDSv1"""
     # ISO Time
     iso8601Time = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
     for envs in describe_environments(cache)["Environments"]:
+        envArn = envs["EnvironmentArn"]
         envName = envs["EnvironmentName"]
         appName = envs["ApplicationName"]
         # loop through all of the configs and option sets to find what we want
@@ -50,124 +51,163 @@ def elasticbeanstalk_imdsv1_disabled_check(cache: dict, awsAccountId: str, awsRe
             ApplicationName=appName,
             EnvironmentName=envName
         )["ConfigurationSettings"]:
+            # TODO : DELETE THIS
+            optionSets = configs["OptionSettings"]
+            with open('~/ebconfigs.json', 'w') as jsonfile:
+                json.dump(optionSets, jsonfile, indent=4, default=str)
+            # TODO : DELETE THE ABOVE
             for opts in configs["OptionSettings"]:
                 print(opts["OptionName"])
                 if opts["OptionName"] == "DisableIMDSv1":
-                    if opts["Value"] == False:
-                        print('False value')
+                    # this is a failing check
+                    if opts["Value"] == "false":
+                        finding = {
+                            "SchemaVersion": "2018-10-08",
+                            "Id": f"{envArn}/beanstalk-env-imdsv1-disabled-check",
+                            "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+                            "GeneratorId": envArn,
+                            "AwsAccountId": awsAccountId,
+                            "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
+                            "FirstObservedAt": iso8601Time,
+                            "CreatedAt": iso8601Time,
+                            "UpdatedAt": iso8601Time,
+                            "Severity": {"Label": "MEDIUM"},
+                            "Confidence": 99,
+                            "Title": "[ElasticBeanstalk.1] Elastic Beanstalk environments should disable IMDSv1",
+                            "Description": f"Elastic Beanstalk environment {envName} does not disable Instance Metadata Service Version 1 (IMDSv1).  IMDSv2 uses session-oriented requests and mitigates several types of vulnerabilities that could be used to try to access the IMDS. For information about these two methods, see Configuring the instance metadata service in the Amazon EC2 User Guide for Linux Instances. Refer to the remediation instructions if this configuration is not intended.",
+                            "Remediation": {
+                                "Recommendation": {
+                                    "Text": "If you Elastic Beanstalk environment should be configured to use IMDSv2 only refer to the Configuring the instance metadata service on your environment's instances section of the AWS Elastic Beanstalk Developer Guide.",
+                                    "Url": "https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/environments-cfg-ec2-imds.html",
+                                }
+                            },
+                            "ProductFields": {"Product Name": "ElectricEye"},
+                            "Resources": [
+                                {
+                                    "Type": "AwsElasticBeanstalkEnvironment",
+                                    "Id": envArn,
+                                    "Partition": awsPartition,
+                                    "Region": awsRegion,
+                                    "Details": {
+                                        "AwsElasticBeanstalkEnvironment": {
+                                            "ApplicationName": appName,
+                                            "Cname": envs["CNAME"],
+                                            "EnvironmentArn": envArn,
+                                            "EnvironmentId": envs["EnvironmentId"],
+                                            "EnvironmentName": envName,
+                                            "PlatformArn": envs["PlatformArn"],
+                                            "Status": envs["Status"],
+                                            "VersionLabel": envs["VersionLabel"],
+                                            "Tier": {
+                                                "Name": envs["Tier"]["Name"],
+                                                "Type": envs["Tier"]["Type"],
+                                                "Version": envs["Tier"]["Version"]
+                                            }
+                                        }
+                                    }
+                                }
+                            ],
+                            "Compliance": {
+                                "Status": "FAILED",
+                                "RelatedRequirements": [
+                                    "NIST CSF PR.AC-4",
+                                    "NIST SP 800-53 AC-1",
+                                    "NIST SP 800-53 AC-2",
+                                    "NIST SP 800-53 AC-3",
+                                    "NIST SP 800-53 AC-5",
+                                    "NIST SP 800-53 AC-6",
+                                    "NIST SP 800-53 AC-14",
+                                    "NIST SP 800-53 AC-16",
+                                    "NIST SP 800-53 AC-24",
+                                    "AICPA TSC CC6.3",
+                                    "ISO 27001:2013 A.6.1.2",
+                                    "ISO 27001:2013 A.9.1.2",
+                                    "ISO 27001:2013 A.9.2.3",
+                                    "ISO 27001:2013 A.9.4.1",
+                                    "ISO 27001:2013 A.9.4.4",
+                                    "ISO 27001:2013 A.9.4.5"
+                                ]
+                            },
+                            "Workflow": {"Status": "NEW"},
+                            "RecordState": "ACTIVE"
+                        }
+                        yield finding
+                    # this is a passing check
                     else:
-                        print('True value')
+                        finding = {
+                            "SchemaVersion": "2018-10-08",
+                            "Id": f"{envArn}/beanstalk-env-imdsv1-disabled-check",
+                            "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+                            "GeneratorId": envArn,
+                            "AwsAccountId": awsAccountId,
+                            "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
+                            "FirstObservedAt": iso8601Time,
+                            "CreatedAt": iso8601Time,
+                            "UpdatedAt": iso8601Time,
+                            "Severity": {"Label": "INFORMATIONAL"},
+                            "Confidence": 99,
+                            "Title": "[ElasticBeanstalk.1] Elastic Beanstalk environments should disable IMDSv1",
+                            "Description": f"Elastic Beanstalk environment {envName} does not disable Instance Metadata Service Version 1 (IMDSv1).  IMDSv2 uses session-oriented requests and mitigates several types of vulnerabilities that could be used to try to access the IMDS. For information about these two methods, see Configuring the instance metadata service in the Amazon EC2 User Guide for Linux Instances. Refer to the remediation instructions if this configuration is not intended.",
+                            "Remediation": {
+                                "Recommendation": {
+                                    "Text": "If you Elastic Beanstalk environment should be configured to use IMDSv2 only refer to the Configuring the instance metadata service on your environment's instances section of the AWS Elastic Beanstalk Developer Guide.",
+                                    "Url": "https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/environments-cfg-ec2-imds.html",
+                                }
+                            },
+                            "ProductFields": {"Product Name": "ElectricEye"},
+                            "Resources": [
+                                {
+                                    "Type": "AwsElasticBeanstalkEnvironment",
+                                    "Id": envArn,
+                                    "Partition": awsPartition,
+                                    "Region": awsRegion,
+                                    "Details": {
+                                        "AwsElasticBeanstalkEnvironment": {
+                                            "ApplicationName": appName,
+                                            "Cname": envs["CNAME"],
+                                            "EnvironmentArn": envArn,
+                                            "EnvironmentId": envs["EnvironmentId"],
+                                            "EnvironmentName": envName,
+                                            "PlatformArn": envs["PlatformArn"],
+                                            "Status": envs["Status"],
+                                            "VersionLabel": envs["VersionLabel"],
+                                            "Tier": {
+                                                "Name": envs["Tier"]["Name"],
+                                                "Type": envs["Tier"]["Type"],
+                                                "Version": envs["Tier"]["Version"]
+                                            }
+                                        }
+                                    }
+                                }
+                            ],
+                            "Compliance": {
+                                "Status": "PASSED",
+                                "RelatedRequirements": [
+                                    "NIST CSF PR.AC-4",
+                                    "NIST SP 800-53 AC-1",
+                                    "NIST SP 800-53 AC-2",
+                                    "NIST SP 800-53 AC-3",
+                                    "NIST SP 800-53 AC-5",
+                                    "NIST SP 800-53 AC-6",
+                                    "NIST SP 800-53 AC-14",
+                                    "NIST SP 800-53 AC-16",
+                                    "NIST SP 800-53 AC-24",
+                                    "AICPA TSC CC6.3",
+                                    "ISO 27001:2013 A.6.1.2",
+                                    "ISO 27001:2013 A.9.1.2",
+                                    "ISO 27001:2013 A.9.2.3",
+                                    "ISO 27001:2013 A.9.4.1",
+                                    "ISO 27001:2013 A.9.4.4",
+                                    "ISO 27001:2013 A.9.4.5"
+                                ]
+                            },
+                            "Workflow": {"Status": "RESOLVED"},
+                            "RecordState": "ARCHIVED"
+                        }
+                        yield finding
                     # stop the loop after the right option is found
                     break
                 else:
                     continue
 
-'''
-@registry.register_check("ec2")
-def ebs_volume_attachment_check(cache: dict, awsAccountId: str, awsRegion: str, awsPartition: str) -> dict:
-    """[EBS.1] EBS Volumes should be in an attached state"""
-    # ISO Time
-    iso8601Time = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
-    for volumes in describe_volumes(cache)["Volumes"]:
-        ebsVolumeId = str(volumes["VolumeId"])
-        ebsVolumeArn = f"arn:{awsPartition}:ec2:{awsRegion}:{awsAccountId}/{ebsVolumeId}"
-        ebsAttachments = volumes["Attachments"]
-        for attachments in ebsAttachments:
-            ebsAttachmentState = str(attachments["State"])    
-            if ebsAttachmentState != "attached":
-                finding = {
-                    "SchemaVersion": "2018-10-08",
-                    "Id": ebsVolumeArn + "/ebs-volume-attachment-check",
-                    "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
-                    "GeneratorId": ebsVolumeArn,
-                    "AwsAccountId": awsAccountId,
-                    "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
-                    "FirstObservedAt": iso8601Time,
-                    "CreatedAt": iso8601Time,
-                    "UpdatedAt": iso8601Time,
-                    "Severity": {"Label": "LOW"},
-                    "Confidence": 99,
-                    "Title": "[EBS.1] EBS Volumes should be in an attached state",
-                    "Description": "EBS Volume "
-                    + ebsVolumeId
-                    + " is not in an attached state. Refer to the remediation instructions if this configuration is not intended",
-                    "Remediation": {
-                        "Recommendation": {
-                            "Text": "If your EBS volume should be attached refer to the Attaching an Amazon EBS Volume to an Instance section of the Amazon Elastic Compute Cloud User Guide",
-                            "Url": "https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ebs-attaching-volume.html",
-                        }
-                    },
-                    "ProductFields": {"Product Name": "ElectricEye"},
-                    "Resources": [
-                        {
-                            "Type": "AwsEc2Volume",
-                            "Id": ebsVolumeArn,
-                            "Partition": awsPartition,
-                            "Region": awsRegion
-                        }
-                    ],
-                    "Compliance": {
-                        "Status": "FAILED",
-                        "RelatedRequirements": [
-                            "NIST CSF ID.AM-2",
-                            "NIST SP 800-53 CM-8",
-                            "NIST SP 800-53 PM-5",
-                            "AICPA TSC CC3.2",
-                            "AICPA TSC CC6.1",
-                            "ISO 27001:2013 A.8.1.1",
-                            "ISO 27001:2013 A.8.1.2",
-                            "ISO 27001:2013 A.12.5.1",
-                        ],
-                    },
-                    "Workflow": {"Status": "NEW"},
-                    "RecordState": "ACTIVE",
-                }
-                yield finding
-            else:
-                finding = {
-                    "SchemaVersion": "2018-10-08",
-                    "Id": ebsVolumeArn + "/ebs-volume-attachment-check",
-                    "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
-                    "GeneratorId": ebsVolumeArn,
-                    "AwsAccountId": awsAccountId,
-                    "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
-                    "FirstObservedAt": iso8601Time,
-                    "CreatedAt": iso8601Time,
-                    "UpdatedAt": iso8601Time,
-                    "Severity": {"Label": "INFORMATIONAL"},
-                    "Confidence": 99,
-                    "Title": "[EBS.1] EBS Volumes should be in an attached state",
-                    "Description": "EBS Volume " + ebsVolumeId + " is in an attached state.",
-                    "Remediation": {
-                        "Recommendation": {
-                            "Text": "If your EBS volume should be attached refer to the Attaching an Amazon EBS Volume to an Instance section of the Amazon Elastic Compute Cloud User Guide",
-                            "Url": "https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ebs-attaching-volume.html",
-                        }
-                    },
-                    "ProductFields": {"Product Name": "ElectricEye"},
-                    "Resources": [
-                        {
-                            "Type": "AwsEc2Volume",
-                            "Id": ebsVolumeArn,
-                            "Partition": awsPartition,
-                            "Region": awsRegion
-                        }
-                    ],
-                    "Compliance": {
-                        "Status": "PASSED",
-                        "RelatedRequirements": [
-                            "NIST CSF ID.AM-2",
-                            "NIST SP 800-53 CM-8",
-                            "NIST SP 800-53 PM-5",
-                            "AICPA TSC CC3.2",
-                            "AICPA TSC CC6.1",
-                            "ISO 27001:2013 A.8.1.1",
-                            "ISO 27001:2013 A.8.1.2",
-                            "ISO 27001:2013 A.12.5.1"
-                        ]
-                    },
-                    "Workflow": {"Status": "RESOLVED"},
-                    "RecordState": "ARCHIVED"
-                }
-                yield finding
-'''
+#
