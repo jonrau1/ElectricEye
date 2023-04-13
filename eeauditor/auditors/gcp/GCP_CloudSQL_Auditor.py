8846f241aca7fa6d41dd29bd4ac21a333a2388b5
jonrau1 <46727149+jonrau1@users.noreply.github.com>
2023-04-13 17:37:52 -0400

    staging a lot of csql checks

--- eeauditor/auditors/gcp/GCP_CloudSQL_Auditor.py
+++ eeauditor/auditors/gcp/GCP_CloudSQL_Auditor.py
@@ -28,39 +28,17 @@ def get_cloudsql_dbs(cache: dict, gcpProjectId: str):
     '''
     AggregatedList result provides Zone information as well as every single Instance in a Project
     '''
-    if cache:
-        return cache
+    response = cache.get("get_cloudsql_dbs")
+    if response:
+        return response
+
+    #  CloudSQL requires SQL Admin API - also doesnt need an aggregatedList
+    service = googleapiclient.discovery.build('sqladmin', 'v1beta4')
+    instances = service.instances().list(project=gcpProjectId).execute()
     
-    results = []
+    cache["get_cloudsql_dbs"] = instances["items"]
 
-    cloudsql = googleapiclient.discovery.build('sqladmin', 'v1beta4')
-
-    aggResult = cloudsql.instances().aggregatedList(project=gcpProjectId).execute()
-
-    import json
-
-    with open('./agg_csql.json', 'w') as jsonfile:
-        json.dump(aggResult, jsonfile, indent=2)
-
-    # Write all Zones to list
-    zoneList = []
-    for zone in aggResult["items"].keys():
-        zoneList.append(zone)
-
-    # If the Zone has a top level key of "warning" it does not contain entries
-    for z in zoneList:
-        for agg in aggResult["items"][z]:
-            if agg == 'warning':
-                continue
-            # reloop the list except looking at instances - this is a normal List we can loop
-            else:
-                for i in aggResult["items"][z]["instances"]:
-                    results.append(i)
-
-    del aggResult
-    del zoneList
-
-    return results
+    return cache["get_cloudsql_dbs"]
 
 @registry.register_check("gce")
 def cloudsql_instance_public_check(cache: dict, awsAccountId: str, awsRegion: str, awsPartition: str, gcpProjectId: str):
@@ -69,151 +47,46 @@ def cloudsql_instance_public_check(cache: dict, awsAccountId: str, awsRegion: st
     """
     iso8601Time = datetime.datetime.now(datetime.timezone.utc).isoformat()
 
-    get_cloudsql_dbs(cache, gcpProjectId)
-
-
-    """for gce in get_cloudsql_dbs(cache, gcpProjectId):
-        id = gce["id"]
-        name = gce["name"]
-        description = gce["description"]
-        zone = gce["zone"].split('/')[-1]
-        machineType = gce["machineType"].split('/')[-1]
-        createdAt = gce["creationTimestamp"]
-        lastStartedAt = gce["lastStartTimestamp"]
-        status = gce["status"]
-        if gce["deletionProtection"] is False:
-            finding = {
-                "SchemaVersion": "2018-10-08",
-                "Id": f"{gcpProjectId}/{zone}/{id}/gce-instance-del-protection-check",
-                "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
-                "GeneratorId": f"{gcpProjectId}/{zone}/{id}/gce-instance-del-protection-check",
-                "AwsAccountId": awsAccountId,
-                "Types": ["Software and Configuration Checks"],
-                "FirstObservedAt": iso8601Time,
-                "CreatedAt": iso8601Time,
-                "UpdatedAt": iso8601Time,
-                "Severity": {"Label": "LOW"},
-                "Confidence": 99,
-                "Title": "[GCP.GCE.1] Google Compute Engine VM instances should have deletion protection enabled",
-                "Description": f"Google Compute Engine instance {name} in {zone} does not have deletion protection enabled. As part of your workload, there might be certain VM instances that are critical to running your application or services, such as an instance running a SQL server, a server used as a license manager, and so on. These VM instances might need to stay running indefinitely so you need a way to protect these VMs from being deleted. With Deletion Protection enabled, you have the guarantee that your VM instances cannot be accidentally deleted. Refer to the remediation instructions if this configuration is not intended.",
-                "Remediation": {
-                    "Recommendation": {
-                        "Text": "If your GCE VM instance should have deletion protection enabled refer to the Prevent accidental VM deletion section of the GCP Compute Engine guide.",
-                        "Url": "https://cloud.google.com/compute/docs/instances/preventing-accidental-vm-deletion",
-                    }
-                },
-                "ProductFields": {
-                    "ProductName": "ElectricEye",
-                    "Provider": "GCP"
-                },
-                "Resources": [
-                    {
-                        "Type": "GcpGceVmInstance",
-                        "Id": f"{id}",
-                        "Partition": awsPartition,
-                        "Region": awsRegion,
-                        "Details": {
-                            "Other": {
-                                "GcpProjectId": gcpProjectId,
-                                "Zone": zone,
-                                "Name": name,
-                                "Id": id,
-                                "Description": description,
-                                "MachineType": machineType,
-                                "CreatedAt": createdAt,
-                                "LastStartedAt": lastStartedAt,
-                                "Status": status
-                            }
-                        },
-                    }
-                ],
-                "Compliance": {
-                    "Status": "FAILED",
-                    "RelatedRequirements": [
-                        "NIST CSF ID.BE-5",
-                        "NIST CSF PR.PT-5",
-                        "NIST SP 800-53 CP-2",
-                        "NIST SP 800-53 CP-11",
-                        "NIST SP 800-53 SA-13",
-                        "NIST SP 800-53 SA14",
-                        "AICPA TSC CC3.1",
-                        "AICPA TSC A1.2",
-                        "ISO 27001:2013 A.11.1.4",
-                        "ISO 27001:2013 A.17.1.1",
-                        "ISO 27001:2013 A.17.1.2",
-                        "ISO 27001:2013 A.17.2.1",
-                    ]
-                },
-                "Workflow": {"Status": "NEW"},
-                "RecordState": "ACTIVE"
-            }
-            yield finding    
+    for csql in get_cloudsql_dbs(cache, gcpProjectId):
+        name = csql["name"]
+        zone = csql["gceZone"]
+        databaseInstalledVersion = csql["databaseInstalledVersion"]
+        createTime = csql["createTime"]
+        state = csql["state"]
+        maintenanceVersion = csql["maintenanceVersion"]
+        ipAddress = csql["ipAddresses"][0]["ipAddress"]
+        # If this value is True, it means a Public IP is assigned
+        if csql["ipConfiguration"]["ipv4Enabled"] == True:
+            print(f"{name} in {zone} is public")
         else:
-            finding = {
-                "SchemaVersion": "2018-10-08",
-                "Id": f"{gcpProjectId}/{zone}/{id}/gce-instance-del-protection-check",
-                "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
-                "GeneratorId": f"{gcpProjectId}/{zone}/{id}/gce-instance-del-protection-check",
-                "AwsAccountId": awsAccountId,
-                "Types": ["Software and Configuration Checks"],
-                "FirstObservedAt": iso8601Time,
-                "CreatedAt": iso8601Time,
-                "UpdatedAt": iso8601Time,
-                "Severity": {"Label": "INFORMATIONAL"},
-                "Confidence": 99,
-                "Title": "[GCP.GCE.1] Google Compute Engine VM instances should have deletion protection enabled",
-                "Description": f"Google Compute Engine instance {name} in {zone} has deletion protection enabled.",
-                "Remediation": {
-                    "Recommendation": {
-                        "Text": "If your GCE VM instance should have deletion protection enabled refer to the Prevent accidental VM deletion section of the GCP Compute Engine guide.",
-                        "Url": "https://cloud.google.com/compute/docs/instances/preventing-accidental-vm-deletion",
-                    }
-                },
-                "ProductFields": {
-                    "ProductName": "ElectricEye",
-                    "Provider": "GCP"
-                },
-                "Resources": [
-                    {
-                        "Type": "GcpGceVmInstance",
-                        "Id": f"{id}",
-                        "Partition": awsPartition,
-                        "Region": awsRegion,
-                        "Details": {
-                            "Other": {
-                                "GcpProjectId": gcpProjectId,
-                                "Zone": zone,
-                                "Name": name,
-                                "Id": id,
-                                "Description": description,
-                                "MachineType": machineType,
-                                "CreatedAt": createdAt,
-                                "LastStartedAt": lastStartedAt,
-                                "Status": status
-                            }
-                        },
-                    }
-                ],
-                "Compliance": {
-                    "Status": "PASSED",
-                    "RelatedRequirements": [
-                        "NIST CSF ID.BE-5",
-                        "NIST CSF PR.PT-5",
-                        "NIST SP 800-53 CP-2",
-                        "NIST SP 800-53 CP-11",
-                        "NIST SP 800-53 SA-13",
-                        "NIST SP 800-53 SA14",
-                        "AICPA TSC CC3.1",
-                        "AICPA TSC A1.2",
-                        "ISO 27001:2013 A.11.1.4",
-                        "ISO 27001:2013 A.17.1.1",
-                        "ISO 27001:2013 A.17.1.2",
-                        "ISO 27001:2013 A.17.2.1",
-                    ]
-                },
-                "Workflow": {"Status": "RESOLVED"},
-                "RecordState": "ARCHIVED"
-            }
-            yield finding"""
+            print(f"{name} in {zone} is private ip")
+
+# Backup Check backupConfiguration.enabled
+
+# PITR MySQL Check backupConfiguration.binaryLogEnabled 
+
+# PITR Postgresql Check backupConfiguration.pointInTimeRecoveryEnabled 
+
+# Private Path Access ipConfiguration.enablePrivatePathForGoogleCloudServices
+
+# Password Policy Enabled passwordValidationPolicy.enablePasswordPolicy
+
+# Password Policy min length CIS (14) passwordValidationPolicy.minLength
+
+# Password Policy Reuse CIS .reuseInterval
+
+# Password Policy disallow username in PW .disallowUsernameSubstring
+
+# Password Policy change interval .passwordChangeInterval
+
+# Storage Autoresize storageAutoResize
+
+# Deletion Protection deletionProtectionEnabled
+
+# Enable Insights Config ... "insightsConfig": {},
+
+# For Insights Config, Log Client IP insightsConfig.recordClientAddress
+
+# Enforce SSL Connections ipConfiguration.requireSsl - can be missing 
 
 # To be continued...?
