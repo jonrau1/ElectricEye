9534bc06c188f816ac867b979174a358e42322ea
jonrau1 <46727149+jonrau1@users.noreply.github.com>
2023-05-03 23:43:14 -0400

    final postgresql update

--- eeauditor/processor/outputs/postgresql.py
+++ eeauditor/processor/outputs/postgresql.py
@@ -140,6 +140,7 @@ class PostgresProvider(object):
                 )
             """)
 
+            print(f"Attempting to write {len(processedFindings)} to PostgreSQL.")
             for f in processedFindings:
 
                 cursor.execute(f"""
@@ -216,83 +217,8 @@ class PostgresProvider(object):
             print("Cannot connect to PostgreSQL! Review your Security Group settings and/or information provided to connect")
             raise oe
         except Exception as e:
-            raise e 
+            raise e
 
-        '''
-        print(f"Writing {len(findings)} results to PostgreSQL")
-        if (self.db_endpoint and self.db_port and self.db_username and self.db_password and self.db_name):
-            try:
-                # Connect to DB and create a Cursor
-                engine = psql.connect(
-                    database=self.db_name,
-                    user=self.db_username,
-                    password=self.db_password,
-                    host=self.db_endpoint,
-                    port=self.db_port
-                )
-                cursor = engine.cursor()
-                
-                # drop previously existing tables
-                cursor.execute("""DROP TABLE IF EXISTS electriceye_findings""")
-                engine.commit()
-                
-                # Create a new table for the ElectricEye findings. Everything is set as Text
-                cursor.execute("""CREATE TABLE IF NOT EXISTS electriceye_findings( schemaversion TEXT, findingid TEXT, awsaccountid TEXT, productarn TEXT, generatorid TEXT, types TEXT, createdat TEXT, severitylabel TEXT, confidence TEXT, title TEXT, description TEXT, resourcetype TEXT, resourceid TEXT, resourceregion TEXT, resourcepartition TEXT, compliancestatus TEXT, compliancecontrols TEXT, workflowstatus TEXT, recordstate TEXT);""")
-
-                for finding in findings:
-                    # Basic parsing of ASFF to prepare for INSERT into PSQL
-                    try:
-                        awsaccountid = str(finding['AwsAccountId'])
-                    except Exception as e:
-                        if str(e) == "'AwsAccountId'":
-                            awsaccountid = str(finding['awsAccountId'])
-                        else:
-                            continue
-                    schemaversion = str(finding['SchemaVersion'])
-                    findingid = str(finding['Id'])
-                    productarn = str(finding['ProductArn'])
-                    generatorid = str(finding['GeneratorId'])
-                    types = str(finding['Types'][0])
-                    createdat = str(finding['CreatedAt'])
-                    severitylabel = str(finding['Severity']['Label'])
-                    #TODO: Find which findings aren't mapped...
-                    try:
-                        confidence = str(finding['Confidence'])
-                    except Exception:
-                        confidence = '99'
-                    title = str(finding['Title'])
-                    description = str(finding['Description'])
-                    resourcetype = str(finding['Resources'][0]['Type'])
-                    resourceid = str(finding['Resources'][0]['Id'])
-                    resourceregion = str(finding['Resources'][0]['Region'])
-                    resourcepartition = str(finding['Resources'][0]['Partition'])
-                    compliancestatus = str(finding['Compliance']['Status'])
-                    #TODO: Find which findings aren't mapped...
-                    try:
-                        compliancecontrols = str(finding['Compliance']['RelatedRequirements'])
-                    except Exception:
-                        compliancecontrols = str('[]')
-                    workflowstatus = str(finding['Workflow']['Status'])
-                    recordstate = str(finding['RecordState'])
-
-                    # Write into Postgres
-                    cursor.execute("INSERT INTO electriceye_findings( schemaversion, findingid, awsaccountid, productarn, generatorid, types, createdat, severitylabel, confidence, title, description, resourcetype, resourceid, resourceregion, resourcepartition, compliancestatus, compliancecontrols, workflowstatus, recordstate) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);", (schemaversion, findingid, awsaccountid, productarn, generatorid, types, createdat, severitylabel, confidence, title, description, resourcetype, resourceid, resourceregion, resourcepartition, compliancestatus, compliancecontrols, workflowstatus, recordstate))
-                
-                # commit the changes
-                engine.commit()
-                # close communication with the postgres server (rds)
-                cursor.close()
-
-            except psql.OperationalError:
-                print("Cannot connect to PostgreSQL! Review your Security Group settings and/or information provided to connect")
-                exit(2)
-            except Exception:
-                print("Another exception found " + Exception)
-                exit(2)
-        else:
-            raise ValueError("Missing credentials or database parameters")
-        '''
-        
     def get_credential_from_aws_ssm(self, value, configurationName):
         """
         Retrieves a TOML variable from AWS Systems Manager Parameter Store and returns it
