d6131136f55aa1e4c562653ec7192356191b4e8b
jonrau1 <46727149+jonrau1@users.noreply.github.com>
2023-05-08 19:30:50 -0400

    Pass TA check on `KeyError` - no resources = pass

--- eeauditor/auditors/aws/AWS_TrustedAdvisor_Auditor.py
+++ eeauditor/auditors/aws/AWS_TrustedAdvisor_Auditor.py
@@ -211,6 +211,83 @@ def trusted_advisor_failing_root_mfa_check(cache: dict, session, awsAccountId: s
             print('You are not subscribed to AWS Premium Support - cannot use the Trusted Advisor Auditor')
         else:
             print(error)
+    # this is a passing check due to no matches
+    except KeyError:
+        finding = {
+            "SchemaVersion": "2018-10-08",
+            "Id": f"{checkArn}/trusted-advisor-failing-root-mfa-check",
+            "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+            "GeneratorId": awsAccountId + checkId,
+            "AwsAccountId": awsAccountId,
+            "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
+            "FirstObservedAt": iso8601Time,
+            "CreatedAt": iso8601Time,
+            "UpdatedAt": iso8601Time,
+            "Severity": {"Label": "INFORMATIONAL"},
+            "Confidence": 99,
+            "Title": "[TrustedAdvisor.1] Trusted Advisor check results for MFA on Root Account should be investigated",
+            "Description": "Trusted Advisor Check for MFA on Root Account with a Check Id of "
+            + checkId
+            + ". is passing.",
+            "Remediation": {
+                "Recommendation": {
+                    "Text": "To learn more about setting up MFA refer to the Using multi-factor authentication (MFA) in AWS section of the AWS Identity and Access Management User Guide.",
+                    "Url": "https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_mfa.html"
+                }
+            },
+            "ProductFields": {
+                "ProductName": "ElectricEye",
+                "Provider": "AWS",
+                "ProviderType": "CSP",
+                "ProviderAccountId": awsAccountId,
+                "AssetRegion": awsRegion,
+                "AssetDetails": assetB64,
+                "AssetClass": "Management & Governance",
+                "AssetService": "AWS Trusted Advisor",
+                "AssetComponent": "Check"
+            },
+            "SourceUrl": "https://console.aws.amazon.com/trustedadvisor/home?region=us-east-1#/category/security",
+            "Resources": [
+                {
+                    "Type": "AwsTrustedAdvisorCheck",
+                    "Id": checkArn,
+                    "Partition": awsPartition,
+                    "Region": awsRegion
+                }
+            ],
+            "Compliance": {
+                "Status": "PASSED",
+                "RelatedRequirements": [
+                    "NIST CSF V1.1 PR.AC-1",
+                    "NIST SP 800-53 Rev. 4 AC-1",
+                    "NIST SP 800-53 Rev. 4 AC-2",
+                    "NIST SP 800-53 Rev. 4 IA-1",
+                    "NIST SP 800-53 Rev. 4 IA-2",
+                    "NIST SP 800-53 Rev. 4 IA-3",
+                    "NIST SP 800-53 Rev. 4 IA-4",
+                    "NIST SP 800-53 Rev. 4 IA-5",
+                    "NIST SP 800-53 Rev. 4 IA-6",
+                    "NIST SP 800-53 Rev. 4 IA-7",
+                    "NIST SP 800-53 Rev. 4 IA-8",
+                    "NIST SP 800-53 Rev. 4 IA-9",
+                    "NIST SP 800-53 Rev. 4 IA-10",
+                    "NIST SP 800-53 Rev. 4 IA-11",
+                    "AICPA TSC CC6.1",
+                    "AICPA TSC CC6.2",
+                    "ISO 27001:2013 A.9.2.1",
+                    "ISO 27001:2013 A.9.2.2",
+                    "ISO 27001:2013 A.9.2.3",
+                    "ISO 27001:2013 A.9.2.4",
+                    "ISO 27001:2013 A.9.2.6",
+                    "ISO 27001:2013 A.9.3.1",
+                    "ISO 27001:2013 A.9.4.2",
+                    "ISO 27001:2013 A.9.4.3"
+                ]
+            },
+            "Workflow": {"Status": "RESOLVED"},
+            "RecordState": "ARCHIVED"
+        }
+        yield finding
 
 @registry.register_check("support")
 def trusted_advisor_failing_elb_listener_security_check(cache: dict, session, awsAccountId: str, awsRegion: str, awsPartition: str) -> dict:
@@ -363,6 +440,70 @@ def trusted_advisor_failing_elb_listener_security_check(cache: dict, session, aw
             print('You are not subscribed to AWS Premium Support - cannot use the Trusted Advisor Auditor')
         else:
             print(error)
+    # this is a passing check due to no matches
+    except KeyError:
+        finding = {
+            "SchemaVersion": "2018-10-08",
+            "Id": f"{checkArn}/trusted-advisor-failing-elb-listener-security-check",
+            "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+            "GeneratorId": awsAccountId + checkId,
+            "AwsAccountId": awsAccountId,
+            "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
+            "FirstObservedAt": iso8601Time,
+            "CreatedAt": iso8601Time,
+            "UpdatedAt": iso8601Time,
+            "Severity": {"Label": "INFORMATIONAL"},
+            "Confidence": 99,
+            "Title": "[TrustedAdvisor.2] Trusted Advisor check results for ELB Listener Security should be investigated",
+            "Description": "Trusted Advisor Check for ELB Listener Security with a Check Id of "
+            + checkId
+            + ". is passing.",
+            "Remediation": {
+                "Recommendation": {
+                    "Text": "To learn more about setting up TLS/SSL for ELBv1 refer to the Listeners for your Classic Load Balancer section of the Elastic Load Balancing Classic Load Balancers User Guide.",
+                    "Url": "https://docs.aws.amazon.com/elasticloadbalancing/latest/classic/elb-listener-config.html#elb-listener-protocols"
+                }
+            },
+            "ProductFields": {
+                "ProductName": "ElectricEye",
+                "Provider": "AWS",
+                "ProviderType": "CSP",
+                "ProviderAccountId": awsAccountId,
+                "AssetRegion": awsRegion,
+                "AssetDetails": assetB64,
+                "AssetClass": "Management & Governance",
+                "AssetService": "AWS Trusted Advisor",
+                "AssetComponent": "Check"
+            },
+            "SourceUrl": "https://console.aws.amazon.com/trustedadvisor/home?region=us-east-1#/category/security",
+            "Resources": [
+                {
+                    "Type": "AwsTrustedAdvisorCheck",
+                    "Id": checkArn,
+                    "Partition": awsPartition,
+                    "Region": awsRegion
+                }
+            ],
+            "Compliance": {
+                "Status": "PASSED",
+                    "RelatedRequirements": [
+                    "NIST CSF V1.1 PR.DS-2",
+                    "NIST SP 800-53 Rev. 4 SC-8",
+                    "NIST SP 800-53 Rev. 4 SC-11",
+                    "NIST SP 800-53 Rev. 4 SC-12",
+                    "AICPA TSC CC6.1",
+                    "ISO 27001:2013 A.8.2.3",
+                    "ISO 27001:2013 A.13.1.1",
+                    "ISO 27001:2013 A.13.2.1",
+                    "ISO 27001:2013 A.13.2.3",
+                    "ISO 27001:2013 A.14.1.2",
+                    "ISO 27001:2013 A.14.1.3"
+                ]
+            },
+            "Workflow": {"Status": "RESOLVED"},
+            "RecordState": "ARCHIVED"
+        }
+        yield finding
 
 @registry.register_check("support")
 def trusted_advisor_failing_cloudfront_ssl_cert_iam_certificate_store_check(cache: dict, session, awsAccountId: str, awsRegion: str, awsPartition: str) -> dict:
@@ -515,6 +656,70 @@ def trusted_advisor_failing_cloudfront_ssl_cert_iam_certificate_store_check(cach
             print('You are not subscribed to AWS Premium Support - cannot use the Trusted Advisor Auditor')
         else:
             print(error)
+    # this is a passing check due to no matches
+    except KeyError:
+        finding = {
+            "SchemaVersion": "2018-10-08",
+            "Id": f"{checkArn}/trusted-advisor-failing-cloudfront-ssl-cert-iam-cert-store-check",
+            "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+            "GeneratorId": awsAccountId + checkId,
+            "AwsAccountId": awsAccountId,
+            "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
+            "FirstObservedAt": iso8601Time,
+            "CreatedAt": iso8601Time,
+            "UpdatedAt": iso8601Time,
+            "Severity": {"Label": "INFORMATIONAL"},
+            "Confidence": 99,
+            "Title": "[TrustedAdvisor.3] Trusted Advisor check results for CloudFront Custom SSL Certificates in the IAM Certificate Store should be investigated",
+            "Description": "Trusted Advisor Check for CloudFront Custom SSL Certificates in the IAM Certificate Store with a Check Id of "
+            + checkId
+            + ". is passing.",
+            "Remediation": {
+                "Recommendation": {
+                    "Text": "To learn more about setting up HTTPS for CloudFront refer to the Using HTTPS with CloudFront section of the Amazon CloudFront Developer Guide.",
+                    "Url": "https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-https.html"
+                }
+            },
+            "ProductFields": {
+                "ProductName": "ElectricEye",
+                "Provider": "AWS",
+                "ProviderType": "CSP",
+                "ProviderAccountId": awsAccountId,
+                "AssetRegion": awsRegion,
+                "AssetDetails": assetB64,
+                "AssetClass": "Management & Governance",
+                "AssetService": "AWS Trusted Advisor",
+                "AssetComponent": "Check"
+            },
+            "SourceUrl": "https://console.aws.amazon.com/trustedadvisor/home?region=us-east-1#/category/security",
+            "Resources": [
+                {
+                    "Type": "AwsTrustedAdvisorCheck",
+                    "Id": checkArn,
+                    "Partition": awsPartition,
+                    "Region": awsRegion
+                }
+            ],
+            "Compliance": {
+                "Status": "PASSED",
+                    "RelatedRequirements": [
+                    "NIST CSF V1.1 PR.DS-2",
+                    "NIST SP 800-53 Rev. 4 SC-8",
+                    "NIST SP 800-53 Rev. 4 SC-11",
+                    "NIST SP 800-53 Rev. 4 SC-12",
+                    "AICPA TSC CC6.1",
+                    "ISO 27001:2013 A.8.2.3",
+                    "ISO 27001:2013 A.13.1.1",
+                    "ISO 27001:2013 A.13.2.1",
+                    "ISO 27001:2013 A.13.2.3",
+                    "ISO 27001:2013 A.14.1.2",
+                    "ISO 27001:2013 A.14.1.3"
+                ]
+            },
+            "Workflow": {"Status": "RESOLVED"},
+            "RecordState": "ARCHIVED"
+        }
+        yield finding
 
 @registry.register_check("support")
 def trusted_advisor_failing_cloudfront_ssl_cert_on_origin_check(cache: dict, session, awsAccountId: str, awsRegion: str, awsPartition: str) -> dict:
@@ -667,6 +872,70 @@ def trusted_advisor_failing_cloudfront_ssl_cert_on_origin_check(cache: dict, ses
             print('You are not subscribed to AWS Premium Support - cannot use the Trusted Advisor Auditor')
         else:
             print(error)
+    # this is a passing check due to no matches
+    except KeyError:
+        finding = {
+            "SchemaVersion": "2018-10-08",
+            "Id": f"{checkArn}/trusted-advisor-failing-cloudfront-ssl-origin-check",
+            "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+            "GeneratorId": awsAccountId + checkId,
+            "AwsAccountId": awsAccountId,
+            "Types": ["Software and Configuration Checks/AWS Security Best Practices"],
+            "FirstObservedAt": iso8601Time,
+            "CreatedAt": iso8601Time,
+            "UpdatedAt": iso8601Time,
+            "Severity": {"Label": "INFORMATIONAL"},
+            "Confidence": 99,
+            "Title": "[TrustedAdvisor.4] Trusted Advisor check results for CloudFront SSL Certificate on the Origin Server should be investigated",
+            "Description": "Trusted Advisor Check for CloudFront SSL Certificate on the Origin Server with a Check Id of "
+            + checkId
+            + ". is passing.",
+            "Remediation": {
+                "Recommendation": {
+                    "Text": "To learn more about setting up HTTPS for CloudFront refer to the Using HTTPS with CloudFront section of the Amazon CloudFront Developer Guide.",
+                    "Url": "https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-https.html"
+                }
+            },
+            "ProductFields": {
+                "ProductName": "ElectricEye",
+                "Provider": "AWS",
+                "ProviderType": "CSP",
+                "ProviderAccountId": awsAccountId,
+                "AssetRegion": awsRegion,
+                "AssetDetails": assetB64,
+                "AssetClass": "Management & Governance",
+                "AssetService": "AWS Trusted Advisor",
+                "AssetComponent": "Check"
+            },
+            "SourceUrl": "https://console.aws.amazon.com/trustedadvisor/home?region=us-east-1#/category/security",
+            "Resources": [
+                {
+                    "Type": "AwsTrustedAdvisorCheck",
+                    "Id": checkArn,
+                    "Partition": awsPartition,
+                    "Region": awsRegion
+                }
+            ],
+            "Compliance": {
+                "Status": "PASSED",
+                    "RelatedRequirements": [
+                    "NIST CSF V1.1 PR.DS-2",
+                    "NIST SP 800-53 Rev. 4 SC-8",
+                    "NIST SP 800-53 Rev. 4 SC-11",
+                    "NIST SP 800-53 Rev. 4 SC-12",
+                    "AICPA TSC CC6.1",
+                    "ISO 27001:2013 A.8.2.3",
+                    "ISO 27001:2013 A.13.1.1",
+                    "ISO 27001:2013 A.13.2.1",
+                    "ISO 27001:2013 A.13.2.3",
+                    "ISO 27001:2013 A.14.1.2",
+                    "ISO 27001:2013 A.14.1.3"
+                ]
+            },
+            "Workflow": {"Status": "RESOLVED"},
+            "RecordState": "ARCHIVED"
+        }
+        yield finding
 
 @registry.register_check("support")
 def trusted_advisor_failing_exposed_access_keys_check(cache: dict, session, awsAccountId: str, awsRegion: str, awsPartition: str) -> dict:
@@ -851,3 +1120,83 @@ def trusted_advisor_failing_exposed_access_keys_check(cache: dict, session, awsA
             print('You are not subscribed to AWS Premium Support - cannot use the Trusted Advisor Auditor')
         else:
             print(error)
+    # this is a passing check due to no matches
+    except KeyError:
+        finding = {
+            "SchemaVersion": "2018-10-08",
+            "Id": f"{checkArn}/trusted-advisor-expose-iam-keys-check",
+            "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+            "GeneratorId": awsAccountId + checkId,
+            "AwsAccountId": awsAccountId,
+            "Types": [
+                "Software and Configuration Checks/AWS Security Best Practices",
+                "Effects/Data Exposure",
+            ],
+            "FirstObservedAt": iso8601Time,
+            "CreatedAt": iso8601Time,
+            "UpdatedAt": iso8601Time,
+            "Severity": {"Label": "INFORMATIONAL"},
+            "Confidence": 99,
+            "Title": "[TrustedAdvisor.5] Trusted Advisor check results for Exposed Access Keys should be investigated",
+            "Description": "Trusted Advisor Check for Exposed Access Keys with a Check Id of "
+            + checkId
+            + ". has failed. Trusted Advisor checks popular code repositories for access keys that have been exposed to the public and for irregular Amazon Elastic Compute Cloud (Amazon EC2) usage that could be the result of a compromised access key. An access key consists of an access key ID and the corresponding secret access key. Exposed access keys pose a security risk to your account and other users. Refer to the remediation instructions if this configuration is not intended.",
+            "Remediation": {
+                "Recommendation": {
+                    "Text": "To learn more about rotating access keys refer to the Managing access keys for IAM users section of the AWS Identity and Access Management User Guide.",
+                    "Url": "https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-https.html"
+                }
+            },
+            "ProductFields": {
+                "ProductName": "ElectricEye",
+                "Provider": "AWS",
+                "ProviderType": "CSP",
+                "ProviderAccountId": awsAccountId,
+                "AssetRegion": awsRegion,
+                "AssetDetails": assetB64,
+                "AssetClass": "Management & Governance",
+                "AssetService": "AWS Trusted Advisor",
+                "AssetComponent": "Check"
+            },
+            "SourceUrl": "https://console.aws.amazon.com/trustedadvisor/home?region=us-east-1#/category/security",
+            "Resources": [
+                {
+                    "Type": "AwsTrustedAdvisorCheck",
+                    "Id": checkArn,
+                    "Partition": awsPartition,
+                    "Region": awsRegion
+                }
+            ],
+            "Compliance": {
+                "Status": "PASSED",
+                "RelatedRequirements": [
+                    "NIST CSF V1.1 PR.AC-1",
+                    "NIST SP 800-53 Rev. 4 AC-1",
+                    "NIST SP 800-53 Rev. 4 AC-2",
+                    "NIST SP 800-53 Rev. 4 IA-1",
+                    "NIST SP 800-53 Rev. 4 IA-2",
+                    "NIST SP 800-53 Rev. 4 IA-3",
+                    "NIST SP 800-53 Rev. 4 IA-4",
+                    "NIST SP 800-53 Rev. 4 IA-5",
+                    "NIST SP 800-53 Rev. 4 IA-6",
+                    "NIST SP 800-53 Rev. 4 IA-7",
+                    "NIST SP 800-53 Rev. 4 IA-8",
+                    "NIST SP 800-53 Rev. 4 IA-9",
+                    "NIST SP 800-53 Rev. 4 IA-10",
+                    "NIST SP 800-53 Rev. 4 IA-11",
+                    "AICPA TSC CC6.1",
+                    "AICPA TSC CC6.2",
+                    "ISO 27001:2013 A.9.2.1",
+                    "ISO 27001:2013 A.9.2.2",
+                    "ISO 27001:2013 A.9.2.3",
+                    "ISO 27001:2013 A.9.2.4",
+                    "ISO 27001:2013 A.9.2.6",
+                    "ISO 27001:2013 A.9.3.1",
+                    "ISO 27001:2013 A.9.4.2",
+                    "ISO 27001:2013 A.9.4.3"
+                ]
+            },
+            "Workflow": {"Status": "RESOLVED"},
+            "RecordState": "ARCHIVED"
+        }
+        yield finding
