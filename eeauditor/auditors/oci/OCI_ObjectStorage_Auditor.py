1fb83c21e94215440a182b80cbeee4cc3495a264
jonrau1 <46727149+jonrau1@users.noreply.github.com>
2023-05-13 13:10:39 -0400

    Stage OCI Object Store auditor

---
+++ eeauditor/auditors/oci/OCI_ObjectStorage_Auditor.py
@@ -0,0 +1,255 @@
+#This file is part of ElectricEye.
+#SPDX-License-Identifier: Apache-2.0
+
+#Licensed to the Apache Software Foundation (ASF) under one
+#or more contributor license agreements.  See the NOTICE file
+#distributed with this work for additional information
+#regarding copyright ownership.  The ASF licenses this file
+#to you under the Apache License, Version 2.0 (the
+#"License"); you may not use this file except in compliance
+#with the License.  You may obtain a copy of the License at
+
+#http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing,
+#software distributed under the License is distributed on an
+#"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#KIND, either express or implied.  See the License for the
+#specific language governing permissions and limitations
+#under the License.
+
+import os
+import oci
+from oci.config import validate_config
+import datetime
+import base64
+import json
+from check_register import CheckRegister
+
+registry = CheckRegister()
+
+def process_response(responseObject):
+    """
+    Receives an OCI Python SDK `Response` type (differs by service) and returns a JSON object
+    """
+
+    payload = json.loads(
+        str(
+            responseObject
+        )
+    )
+
+    return payload
+
+def get_object_storage_buckets(cache, ociTenancyId, ociUserId, ociRegionName, ociCompartments, ociUserApiKeyFingerprint):
+    
+    response = cache.get("get_object_storage_buckets")
+    if response:
+        return response
+
+    # Create & Validate OCI Creds - do this after cache check to avoid doing it a lot
+    config = {
+        "tenancy": ociTenancyId,
+        "user": ociUserId,
+        "region": ociRegionName,
+        "fingerprint": ociUserApiKeyFingerprint,
+        "key_file": os.environ["OCI_PEM_FILE_PATH"],
+        
+    }
+    validate_config(config)
+
+    objectStorageClient = oci.object_storage.ObjectStorageClient(config)
+
+    aBigListOfBuckets = []
+
+    for compartment in ociCompartments:
+        # Each Oracle Cloud Infrastructure tenant is assigned one unique and uneditable Object Storage namespace.
+        # The namespace is a system-generated string assigned during account creation.
+        namespaceId = str(objectStorageClient.get_namespace(compartment_id=compartment).data)
+        # Namespace is probably the same for every compartment...
+        for bucket in objectStorageClient.list_buckets(namespace_name=namespaceId, compartment_id=compartment).data:
+            bucket = process_response(bucket)
+            bucketName = bucket["name"]
+            # Only the GetBucket API has the extended information, not the ListBucket API
+            bucketInfo = process_response(
+                objectStorageClient.get_bucket(
+                    namespace_name=namespaceId, bucket_name=bucketName
+                ).data
+            )
+            # Get the Lifecycle Policies, if there are not any write any empty list
+            try:
+                lifeCycleInfo = process_response(
+                    objectStorageClient.get_object_lifecycle_policy(
+                        namespace_name=namespaceId, bucket_name=bucketName
+                    ).data
+                )["items"]
+            except oci.exceptions.ServiceError:
+                lifeCycleInfo = []
+            # Insert the lifecycle into a new dict
+            bucketInfo["object_lifecycle_policies"] = lifeCycleInfo
+            # Append the new payload
+            aBigListOfBuckets.append(bucketInfo)
+
+    cache["get_object_storage_buckets"] = aBigListOfBuckets
+    return cache["get_object_storage_buckets"]
+
+# Use Customer-Managed Keys
+@registry.register_check("oci.objectstorage")
+def oci_object_storage_bucket_cmk_mek_check(cache, awsAccountId, awsRegion, awsPartition, ociTenancyId, ociUserId, ociRegionName, ociCompartments, ociUserApiKeyFingerprint):
+    """
+    [OCI.ObjectStorage.1] Object Storage buckets should be encrypted with a Customer-managed Master Encryption Key
+    """
+    # ISO Time
+    iso8601Time = datetime.datetime.now(datetime.timezone.utc).isoformat()
+    for bucket in get_object_storage_buckets(cache, ociTenancyId, ociUserId, ociRegionName, ociCompartments, ociUserApiKeyFingerprint):
+        # B64 encode all of the details for the Asset
+        assetJson = json.dumps(bucket,default=str).encode("utf-8")
+        assetB64 = base64.b64encode(assetJson)
+        compartmentId = bucket["compartment_id"]
+        bucketId = bucket["id"]
+        bucketName = bucket["display_name"]
+        createdAt = str(bucket["time_created"])
+
+        if bucket["kms_key_id"] is None:
+            finding = {
+                "SchemaVersion": "2018-10-08",
+                "Id": f"{ociTenancyId}/{ociRegionName}/{compartmentId}/{bucketId}/oci-object-storage-bucket-cmk-mek-check",
+                "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+                "GeneratorId": f"{ociTenancyId}/{ociRegionName}/{compartmentId}/{bucketId}/oci-object-storage-bucket-cmk-mek-check",
+                "AwsAccountId": awsAccountId,
+                "Types": ["Software and Configuration Checks"],
+                "FirstObservedAt": iso8601Time,
+                "CreatedAt": iso8601Time,
+                "UpdatedAt": iso8601Time,
+                "Severity": {"Label": "LOW"},
+                "Confidence": 99,
+                "Title": "[OCI.ObjectStorage.1] Object Storage buckets should be encrypted with a Customer-managed Master Encryption Key",
+                "Description": f"Oracle Object Storage bucket {bucketName} in Compartment {compartmentId} in {ociRegionName} does not use a Customer-managed Master Encryption Key. The Oracle Cloud Infrastructure Object Storage service encrypts and decrypts all objects using 256-bit Advanced Encryption Standard (AES-256) to encrypt object data on the server. Each object is encrypted with its own data encryption key. Data encryption keys are always encrypted with a master encryption key that is assigned to the bucket. Encryption is enabled by default and cannot be turned off. By default, Oracle manages the master encryption key. The Oracle Cloud Infrastructure Object Storage service encrypts your data and metadata (customer-provided key value pairs) using randomly generated Data Encryption Keys (DEKs). Object Storage allows you to specify your own Master Encryption Key (MEK) managed by the Vault service for buckets and individual objects. You can specify the MEK to be used for a given object without having to maintain and manage your own keys. Refer to the remediation instructions if this configuration is not intended.",
+                "Remediation": {
+                    "Recommendation": {
+                        "Text": "For more information on using a customer-managed MEK for your buckets refer to the Using Your Own Keys in Vault for Server-Side Encryption section of the Oracle Cloud Infrastructure Documentation for Object Storage.",
+                        "Url": "https://docs.oracle.com/en-us/iaas/Content/Object/Tasks/encryption.htm#UsingYourKMSKeys",
+                    }
+                },
+                "ProductFields": {
+                    "ProductName": "ElectricEye",
+                    "Provider": "OCI",
+                    "ProviderType": "CSP",
+                    "ProviderAccountId": ociTenancyId,
+                    "AssetRegion": ociRegionName,
+                    "AssetDetails": assetB64,
+                    "AssetClass": "Storage",
+                    "AssetService": "Oracle Object Storage",
+                    "AssetComponent": "Bucket"
+                },
+                "Resources": [
+                    {
+                        "Type": "OciObjectStorageBucket",
+                        "Id": bucketId,
+                        "Partition": awsPartition,
+                        "Region": awsRegion,
+                        "Details": {
+                            "Other": {
+                                "TenancyId": ociTenancyId,
+                                "CompartmentId": compartmentId,
+                                "Region": ociRegionName,
+                                "Name": bucketName,
+                                "Id": bucketId,
+                                "CreatedAt": createdAt
+                            }
+                        }
+                    }
+                ],
+                "Compliance": {
+                    "Status": "FAILED",
+                    "RelatedRequirements": [
+                        "NIST CSF V1.1 PR.DS-1",
+                        "NIST SP 800-53 Rev. 4 MP-8",
+                        "NIST SP 800-53 Rev. 4 SC-12",
+                        "NIST SP 800-53 Rev. 4 SC-28",
+                        "AICPA TSC CC6.1",
+                        "ISO 27001:2013 A.8.2.3"
+                    ]
+                },
+                "Workflow": {"Status": "NEW"},
+                "RecordState": "ACTIVE"
+            }
+            yield finding
+        else:
+            finding = {
+                "SchemaVersion": "2018-10-08",
+                "Id": f"{ociTenancyId}/{ociRegionName}/{compartmentId}/{bucketId}/oci-object-storage-bucket-cmk-mek-check",
+                "ProductArn": f"arn:{awsPartition}:securityhub:{awsRegion}:{awsAccountId}:product/{awsAccountId}/default",
+                "GeneratorId": f"{ociTenancyId}/{ociRegionName}/{compartmentId}/{bucketId}/oci-object-storage-bucket-cmk-mek-check",
+                "AwsAccountId": awsAccountId,
+                "Types": ["Software and Configuration Checks"],
+                "FirstObservedAt": iso8601Time,
+                "CreatedAt": iso8601Time,
+                "UpdatedAt": iso8601Time,
+                "Severity": {"Label": "INFORMATIONAL"},
+                "Confidence": 99,
+                "Title": "[OCI.ObjectStorage.1] Object Storage buckets should be encrypted with a Customer-managed Master Encryption Key",
+                "Description": f"Oracle Object Storage bucket {bucketName} in Compartment {compartmentId} in {ociRegionName} does use a Customer-managed Master Encryption Key.",
+                "Remediation": {
+                    "Recommendation": {
+                        "Text": "For more information on using a customer-managed MEK for your buckets refer to the Using Your Own Keys in Vault for Server-Side Encryption section of the Oracle Cloud Infrastructure Documentation for Object Storage.",
+                        "Url": "https://docs.oracle.com/en-us/iaas/Content/Object/Tasks/encryption.htm#UsingYourKMSKeys",
+                    }
+                },
+                "ProductFields": {
+                    "ProductName": "ElectricEye",
+                    "Provider": "OCI",
+                    "ProviderType": "CSP",
+                    "ProviderAccountId": ociTenancyId,
+                    "AssetRegion": ociRegionName,
+                    "AssetDetails": assetB64,
+                    "AssetClass": "Storage",
+                    "AssetService": "Oracle Object Storage",
+                    "AssetComponent": "Bucket"
+                },
+                "Resources": [
+                    {
+                        "Type": "OciObjectStorageBucket",
+                        "Id": bucketId,
+                        "Partition": awsPartition,
+                        "Region": awsRegion,
+                        "Details": {
+                            "Other": {
+                                "TenancyId": ociTenancyId,
+                                "CompartmentId": compartmentId,
+                                "Region": ociRegionName,
+                                "Name": bucketName,
+                                "Id": bucketId,
+                                "CreatedAt": createdAt
+                            }
+                        }
+                    }
+                ],
+                "Compliance": {
+                    "Status": "PASSED",
+                    "RelatedRequirements": [
+                        "NIST CSF V1.1 PR.DS-1",
+                        "NIST SP 800-53 Rev. 4 MP-8",
+                        "NIST SP 800-53 Rev. 4 SC-12",
+                        "NIST SP 800-53 Rev. 4 SC-28",
+                        "AICPA TSC CC6.1",
+                        "ISO 27001:2013 A.8.2.3"
+                    ]
+                },
+                "Workflow": {"Status": "RESOLVED"},
+                "RecordState": "ARCHIVED"
+            }
+            yield finding
+
+# Object Lifecycle Policy Configured
+
+# Multipart failed item deletion policy - use get_object_lifecycle_policy and look for 'target': 'multipart-uploads' 'is_enabled': True
+
+# Public Access
+
+# Replication
+
+# Versioning
+
+
+## END ??
